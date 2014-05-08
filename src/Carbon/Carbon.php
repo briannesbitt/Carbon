6dcef6f4cffedc325518034117968dd1e8e9a7a0
doubleking <jablonski.kce@gmail.com>
2014-05-08 10:08:24 +0200

    Self keyword changed to static in all method calls, but all constants references were left with self keyword. It is due to the fact that there are constants, but also because static keyword is not allowed in compile time constants. Therefore static keyword can't be used to declare another constant or method argument.

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -163,9 +163,9 @@ class Carbon extends DateTime
    {
       // If the class has a test now set and we are trying to create a now()
       // instance then override as required
-      if (static::hasTestNow() && (empty($time) || $time === 'now' || self::hasRelativeKeywords($time))) {
+      if (static::hasTestNow() && (empty($time) || $time === 'now' || static::hasRelativeKeywords($time))) {
          $testInstance = clone static::getTestNow();
-         if (self::hasRelativeKeywords($time)) {
+         if (static::hasRelativeKeywords($time)) {
    	     $testInstance->modify($time);
          }
 
@@ -180,7 +180,7 @@ class Carbon extends DateTime
       }
 
       if ($tz !== null) {
-         parent::__construct($time, self::safeCreateDateTimeZone($tz));
+         parent::__construct($time, static::safeCreateDateTimeZone($tz));
       } else {
          parent::__construct($time);
       }
@@ -298,7 +298,7 @@ class Carbon extends DateTime
          $second = ($second === null) ? 0 : $second;
       }
 
-      return self::createFromFormat('Y-n-j G:i:s', sprintf('%s-%s-%s %s:%02s:%02s', $year, $month, $day, $hour, $minute, $second), $tz);
+      return static::createFromFormat('Y-n-j G:i:s', sprintf('%s-%s-%s %s:%02s:%02s', $year, $month, $day, $hour, $minute, $second), $tz);
    }
 
    /**
@@ -313,7 +313,7 @@ class Carbon extends DateTime
     */
    public static function createFromDate($year = null, $month = null, $day = null, $tz = null)
    {
-      return self::create($year, $month, $day, null, null, null, $tz);
+      return static::create($year, $month, $day, null, null, null, $tz);
    }
 
    /**
@@ -328,7 +328,7 @@ class Carbon extends DateTime
     */
    public static function createFromTime($hour = null, $minute = null, $second = null, $tz = null)
    {
-      return self::create(null, null, null, $hour, $minute, $second, $tz);
+      return static::create(null, null, null, $hour, $minute, $second, $tz);
    }
 
    /**
@@ -345,13 +345,13 @@ class Carbon extends DateTime
    public static function createFromFormat($format, $time, $tz = null)
    {
       if ($tz !== null) {
-         $dt = parent::createFromFormat($format, $time, self::safeCreateDateTimeZone($tz));
+         $dt = parent::createFromFormat($format, $time, static::safeCreateDateTimeZone($tz));
       } else {
          $dt = parent::createFromFormat($format, $time);
       }
 
       if ($dt instanceof DateTime) {
-         return self::instance($dt);
+         return static::instance($dt);
       }
 
       $errors = static::getLastErrors();
@@ -368,7 +368,7 @@ class Carbon extends DateTime
     */
    public static function createFromTimestamp($timestamp, $tz = null)
    {
-      return self::now($tz)->setTimestamp($timestamp);
+      return static::now($tz)->setTimestamp($timestamp);
    }
 
    /**
@@ -390,7 +390,7 @@ class Carbon extends DateTime
     */
    public function copy()
    {
-      return self::instance($this);
+      return static::instance($this);
    }
 
    ///////////////////////////////////////////////////////////////////
@@ -719,7 +719,7 @@ class Carbon extends DateTime
     */
    public function setTimezone($value)
    {
-      parent::setTimezone(self::safeCreateDateTimeZone($value));
+      parent::setTimezone(static::safeCreateDateTimeZone($value));
 
       return $this;
    }
@@ -785,7 +785,7 @@ class Carbon extends DateTime
          return false;
       }
 
-      foreach(self::$relativeKeywords as $keyword) {
+      foreach(static::$relativeKeywords as $keyword) {
          if (stripos($time, $keyword) !== false) {
             return true;
         }
@@ -806,7 +806,7 @@ class Carbon extends DateTime
     *
     * @return string
     */
-   public function formatLocalized($format)
+   public function formatLocalized($format = self::COOKIE)
    {
       // Check for Windows to find and replace the %e
       // modifier correctly
@@ -1161,7 +1161,7 @@ class Carbon extends DateTime
     */
    public function isYesterday()
    {
-      return $this->toDateString() === self::now($this->tz)->subDay()->toDateString();
+      return $this->toDateString() === static::now($this->tz)->subDay()->toDateString();
    }
 
    /**
@@ -1171,7 +1171,7 @@ class Carbon extends DateTime
     */
    public function isToday()
    {
-      return $this->toDateString() === self::now($this->tz)->toDateString();
+      return $this->toDateString() === static::now($this->tz)->toDateString();
    }
 
    /**
@@ -1181,7 +1181,7 @@ class Carbon extends DateTime
     */
    public function isTomorrow()
    {
-      return $this->toDateString() === self::now($this->tz)->addDay()->toDateString();
+      return $this->toDateString() === static::now($this->tz)->addDay()->toDateString();
    }
 
    /**
@@ -1191,7 +1191,7 @@ class Carbon extends DateTime
     */
    public function isFuture()
    {
-      return $this->gt(self::now($this->tz));
+      return $this->gt(static::now($this->tz));
    }
 
    /**
