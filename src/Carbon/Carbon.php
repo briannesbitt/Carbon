658765d516a04ae9559d1380af1f2896324b41c1
Lucas Michot <lucas@semalead.com>
2014-08-26 11:41:51 +0200

    Fix psr in src

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -797,7 +797,8 @@ class Carbon extends DateTime
     *
     * @return boolean true if there is a keyword, otherwise false
     */
-   public static function hasRelativeKeywords($time) {
+   public static function hasRelativeKeywords($time)
+   {
       // skip common format with a '-' in it
       if (preg_match('/[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/', $time) === 1) {
          return false;
@@ -1231,7 +1232,7 @@ class Carbon extends DateTime
    {
       return $this->format('L') == '1';
    }
-   
+
    /**
     * Checks if the passed in date is the same day as the instance current day.
     *
