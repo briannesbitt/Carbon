e282ad48fdb1f318607d866c56f8b7686e5f3181
Ronny Unger <ronny.unger@gmx.net>
2014-10-16 19:16:31 +0200

    fixed intentation issue

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -842,7 +842,7 @@ class Carbon extends DateTime
         if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
              $format = preg_replace('#(?<!%)((?:%%)*)%e#', '\1%#d', $format);
         }
-        
+
         return strftime($format, strtotime($this));
     }
 
