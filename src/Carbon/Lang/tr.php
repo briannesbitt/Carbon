3379138899098c2313f84fe26ee18ab9d103b0f1
Baran Şengül <baransengul@outlook.com>
2015-02-25 15:55:01 +0200

    Update and rename src/Carbon/Lang to src/Carbon/Lang/tr.php
    Turkish translation

---
+++ src/Carbon/Lang/tr.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+return array(
+    'year'      => '1 yıl|:count yıl',
+    'month'     => '1 ay|:count ay',
+    'week'      => '1 hafta|:count hafta',
+    'day'       => '1 gün|:count gün',
+    'hour'      => '1 saat|:count saat',
+    'minute'    => '1 dakika|:count dakika',
+    'second'    => '1 saniye|:count saniye',
+    'ago'       => ':time önce',
+    'from_now'  => ':time andan itibaren',
+    'after'     => ':time sonra',
+    'before'    => ':time önce'
+);
