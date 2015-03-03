f8fb4590e6386fbd1c2f38d26953c3005e6b4707
Kyle <kylekatarnls@users.noreply.github.com>
2015-03-03 11:48:15 +0100

    el language
    Add el language from laravel-dat project

---
+++ src/Carbon/Lang/el.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+/**
+ * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/el/date.php
+ */
+return array(
+    'year'      => '1 χρόνος|:count χρόνια',
+    'month'     => '1 μήνας|:count μήνες',
+    'week'      => '1 εβδομάδα|:count εβδομάδες',
+    'day'       => '1 μέρα|:count μέρες',
+    'hour'      => '1 ώρα|:count ώρες',
+    'minute'    => '1 λεπτό|:count λεπτά',
+    'second'    => '1 δευτερόλεπτο|:count δευτερόλεπτα',
+    'ago'       => 'πρίν απο :time',
+    'from_now'  => 'σε :time απο τώρα',
+    'after'     => ':time μετά',
+    'before'    => ':time πρίν'
+);
