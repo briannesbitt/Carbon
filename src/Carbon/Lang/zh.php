a44ba0345dec0de5cf512b53377ef736e22f0002
Kyle <kylekatarnls@users.noreply.github.com>
2015-03-13 11:02:39 +0100

    zh language
    Add zh language from laravel-date project

---
+++ src/Carbon/Lang/zh.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+/**
+ * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/zh/date.php
+ */
+return array(
+    'year'      => ':count年',
+    'month'     => ':count月',
+    'week'      => ':count周',
+    'day'       => ':count天',
+    'hour'      => ':count小时',
+    'minute'    => ':count分钟',
+    'second'    => ':count秒',
+    'ago'       => ':time前',
+    'from_now'  => ':time距现在',
+    'after'     => ':time后',
+    'before'    => ':time前'
+);
