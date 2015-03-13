79117d83539d6728ceefb3113c85a0bb9a6d7e1a
Kyle <kylekatarnls@users.noreply.github.com>
2015-03-13 10:57:13 +0100

    uk language
    Add uk language from laravel-date project

---
+++ src/Carbon/Lang/uk.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+/**
+ * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/uk/date.php
+ */
+return array(
+    'year'      => ':count рік|:count роки|:count років',
+    'month'     => ':count місяць|:count місяці|:count місяців',
+    'week'      => ':count тиждень|:count тижні|:count тижнів',
+    'day'       => ':count день|:count дні|:count днів',
+    'hour'      => ':count година|:count години|:count годин',
+    'minute'    => ':count хвилину|:count хвилини|:count хвилин',
+    'second'    => ':count секунду|:count секунди|:count секунд',
+    'ago'       => ':time назад',
+    'from_now'  => 'через :time',
+    'after'     => ':time після',
+    'before'    => ':time до'
+);
