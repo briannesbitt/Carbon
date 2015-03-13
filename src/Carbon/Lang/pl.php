8e74ff37382df7b1cfff5703ced512c26e4699aa
Kyle <kylekatarnls@users.noreply.github.com>
2015-03-13 09:57:56 +0100

    pl language
    Add pl language from laravel-date project

---
+++ src/Carbon/Lang/pl.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+/**
+ * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/pl/date.php
+ */
+return array(
+    'year'      => '1 rok|:count lata|:count lat',
+    'month'     => '1 miesiąc|:count miesiące|:count miesięcy',
+    'week'      => '1 tydzień|:count tygodnie|:count tygodni',
+    'day'       => '1 dzień|:count dni|:count dni',
+    'hour'      => '1 godzina|:count godziny|:count godzin',
+    'minute'    => '1 minuta|:count minuty|:count minut',
+    'second'    => '1 sekunda|:count sekundy|:count sekund',
+    'ago'       => ':time temu',
+    'from_now'  => ':time od teraz',
+    'after'     => ':time przed',
+    'before'    => ':time po'
+);
