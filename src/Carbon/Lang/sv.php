7946b1973fb704a2a542ce5ac29fceb5ab1e5e7a
Kyle <kylekatarnls@users.noreply.github.com>
2015-03-13 10:48:54 +0100

    sv language
    Add sv language from laravel-date project

---
+++ src/Carbon/Lang/sv.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+/**
+ * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/sv/date.php
+ */
+return array(
+    'year'      => '1 år|:count år',
+    'month'     => '1 månad|:count månader',
+    'week'      => '1 vecka|:count veckor',
+    'day'       => '1 dag|:count dagar',
+    'hour'      => '1 timme|:count timmar',
+    'minute'    => '1 minut|:count minuter',
+    'second'    => '1 sekund|:count sekunder',
+    'ago'       => ':time sedan',
+    'from_now'  => 'om :time',
+    'after'     => ':time efter',
+    'before'    => ':time före'
+);
