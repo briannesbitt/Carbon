1fd9ca1e4571f591cc17f5b308ef02e57c2875bd
Kyle <kylekatarnls@users.noreply.github.com>
2015-03-13 09:41:50 +0100

    id language
    Add id language from laravel-date project

---
+++ src/Carbon/Lang/id.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+/**
+ * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/id/date.php
+ */
+return array(
+    'year'      => ':count tahun',
+    'month'     => ':count bulan',
+    'week'      => ':count minggu',
+    'day'       => ':count hari',
+    'hour'      => ':count jam',
+    'minute'    => ':count menit',
+    'second'    => ':count detik',
+    'ago'       => ':time yang lalu',
+    'from_now'  => ':time dari sekarang',
+    'after'     => ':time setelah',
+    'before'    => ':time sebelum'
+);
