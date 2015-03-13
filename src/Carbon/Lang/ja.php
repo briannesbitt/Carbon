ce82eb67cd5c64f7109bf47fcb356899f0e56ab2
Kyle <kylekatarnls@users.noreply.github.com>
2015-03-13 09:48:52 +0100

    ja language
    Add ja language from laravel-date project

---
+++ src/Carbon/Lang/ja.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+/**
+ * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/ja/date.php
+ */
+return array(
+    'year'      => ':count 年',
+    'month'     => ':count ヶ月',
+    'week'      => ':count 週間',
+    'day'       => ':count 日',
+    'hour'      => ':count 時間',
+    'minute'    => ':count 分',
+    'second'    => ':count 秒',
+    'ago'       => ':time 前',
+    'from_now'  => '今から :time',
+    'after'     => ':time 後',
+    'before'    => ':time 前'
+);
