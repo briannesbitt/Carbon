6e04352ce3d83f67974c2b3e9685c5718f9a2218
nimamo <nima.moatamedi@gmail.com>
2015-03-26 13:24:05 +0430

    fa language
    Add fa language.

---
+++ src/Carbon/Lang/fa.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+
+return array(
+    'year'      => ':count سال',
+    'month'     => ':count ماه',
+    'week'      => ':count هفته',
+    'day'       => ':count روز',
+    'hour'      => ':count ساعت',
+    'minute'    => ':count دقیقه',
+    'second'    => ':count ثانیه',
+    'ago'       => ':time پیش',
+    'from_now'  => ':time بعد',
+    'after'     => ':time پیش از',
+    'before'    => ':time پس از',
+);
