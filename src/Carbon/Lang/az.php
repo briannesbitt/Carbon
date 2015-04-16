6e5e5e9ff6c08790d1773fdf309f3eab23d9c939
Kyle <kylekatarnls@users.noreply.github.com>
2015-04-16 22:03:36 +0200

    Add az language
    Import az language from laravel-date by @The-Hasanov

---
+++ src/Carbon/Lang/az.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+ /**
+ * Extracted from https://github.com/The-Hasanov/laravel-date/blob/1006f37c431178b5c7219d02c93579c9690ae546/src/Lang/az.php
+ */
+return array(
+    'year'      => ':count il',
+    'month'     => ':count ay',
+    'week'      => ':count həftə',
+    'day'       => ':count gün',
+    'hour'      => ':count saat',
+    'minute'    => ':count dəqiqə',
+    'second'    => ':count saniyə',
+    'ago'       => ':time öncə',
+    'from_now'  => ':time sonra',
+    'after'     => ':time sonra',
+    'before'    => ':time öncə'
+);
