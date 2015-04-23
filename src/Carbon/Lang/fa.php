fc4bb8ffd11b9994fd8f1dd6e69c8476ea599c28
Hossein Jabbari <mr.hhkj@gmail.com>
2015-04-23 16:43:25 +0430

    Farsi (Persian) Translation
    Used in Iran, Tajikistan, Afghanistan. Total Persian speaking population globally is around 120 Million.

---
+++ src/Carbon/Lang/fa.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ * Farsi (Persian) translations done by Hossein Jabbari (https://github.com/hosseinhkj)
+ */
+return array(
+    'year'      => '۱ سال|:count سال',
+    'month'     => '۱ ماه|:count ماه',
+    'week'      => '۱ هفته|:count هفته',
+    'day'       => '۱ روز|:count روز',
+    'hour'      => '۱ ساعت|:count ساعت',
+    'minute'    => '۱ دقیقه|:count دقیقه',
+    'second'    => '۱ ثانیه|:count ثانیه',
+    'ago'       => ':time قبل',
+    'from_now'  => ':time دیگر',
+    'after'     => ':time بعد',
+    'before'    => ':time قبل',
+);
