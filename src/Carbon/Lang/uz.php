928eea2a1ea4dbd922f484f263d7374460a11ebe
aarkhipov <aarkhipov1991@gmail.com>
2015-05-26 13:23:03 +0400

    add UZ locale

---
+++ src/Carbon/Lang/uz.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+
+return array(
+    'year'      => ':count yil|:count yil|:count yil',
+    'month'     => ':count oy|:count oy|:count oylar',
+    'week'      => ':count hafta|:count hafta|:count hafta',
+    'day'       => ':count kun|:count kun|:count kun',
+    'hour'      => ':count soat|:count soat|:count soat',
+    'minute'    => ':count minut|:count minut|:count minut',
+    'second'    => ':count sekund|:count sekund|:count sekund',
+    'ago'       => ':time avval',
+    'from_now'  => 'keyin :time',
+    'after'     => ':time keyin',
+    'before'    => ':time gacha'
+);
