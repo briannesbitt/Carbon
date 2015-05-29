234035ec63ff72d05dd782eb1ef9111207ff9416
Faisal Islam <faisal@nascenia.com>
2015-05-29 19:27:33 +0600

    added bangla transalation

---
+++ src/Carbon/Lang/bn.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+/**
+ * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/eu/date.php
+ */
+return array(
+    'year'      => '১ বছর|:count বছর',
+    'month'     => '১ মাস|:count মাস',
+    'week'      => '১ সপ্তাহ|:count সপ্তাহ',
+    'day'       => '১ দিন|:count দিন',
+    'hour'      => '১ ঘন্টা|:count ঘন্টা',
+    'minute'    => '১ মিনিট|:count মিনিট',
+    'second'    => '১ সেকেন্ড|:count সেকেন্ড',
+    'ago'       => ':time পূর্বে',
+    'from_now'  => 'এখন থেকে :time',
+    'after'     => ':time পরে',
+    'before'    => ':time আগে',
+);
