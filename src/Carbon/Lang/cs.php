878c4b85b3a0d87fa4c570aaaea46ef6c0aaaf94
newman101 <jremes@outlook.com>
2015-05-29 21:05:08 +0100

    Updated Czech language file to include numbers

--- src/Carbon/Lang/cs.php
+++ src/Carbon/Lang/cs.php
@@ -16,13 +16,13 @@
  * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/cs/date.php
  */
 return array(
-    'year'      => 'rok|:count roky|:count let',
-    'month'     => 'měsíc|:count měsíce|:count měsíců',
-    'week'      => 'týden|:count týdny|:count týdnů',
-    'day'       => 'den|:count dny|:count dní',
-    'hour'      => 'hodinu|:count hodiny|:count hodin',
-    'minute'    => 'minutu|:count minuty|:count minut',
-    'second'    => 'sekundu|:count sekundy|:count sekund',
+    'year'      => '1 rok|:count roky|:count let',
+    'month'     => '1 měsíc|:count měsíce|:count měsíců',
+    'week'      => '1 týden|:count týdny|:count týdnů',
+    'day'       => '1 den|:count dny|:count dní',
+    'hour'      => '1 hodinu|:count hodiny|:count hodin',
+    'minute'    => '1 minutu|:count minuty|:count minut',
+    'second'    => '1 sekundu|:count sekundy|:count sekund',
     'ago'       => 'před :time',
     'from_now'  => 'za :time',
     'after'     => ':time později',
