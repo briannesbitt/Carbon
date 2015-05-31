56a9e165e9d3ccd569712ce3cbb83b61a0645e93
dani3l <dani3l@gmail.com>
2015-05-31 12:45:55 +0300

    Fix conditions

--- src/Carbon/Lang/he.php
+++ src/Carbon/Lang/he.php
@@ -15,16 +15,16 @@
  */
  
 /**
- * Hebrew translation by sUP <dani3l@gmail.com>
+ * Translated by dani3l <dani3l@gmail.com>
  */
 return array(
-    'year'      => '{0} :count שנים|{1} שנה|{2} שנתיים|[3,Inf] :count שנים',
-    'month'     => '{0} :count חודשים|{1} חודש|{2} חודשיים|[3,Inf] :count חודשים',
-    'week'      => '{0} :count שבועות|{1} שבוע|{2} שבועיים|[3,Inf] :count שבועות',
-    'day'       => '{0} :count ימים|{1} יום|{2} יומיים|[3,Inf] :count ימים',
-    'hour'      => '{0} :count שעות|{1} שעה|{2} שעתיים|[3,Inf] :count שעות',
-    'minute'    => '{0} :count דקות|{1} דקה|[2,Inf] :count דקות',
-    'second'    => '{0} :count שניות|{1} שנייה|[2,Inf] :count שניות',
+    'year'      => 'שנה|{2} שנתיים|:count שנים',
+    'month'     => 'חודש|{2} חודשיים|:count חודשים',
+    'week'      => 'שבוע|{2} שבועיים|:count שבועות',
+    'day'       => 'יום|{2} יומיים|:count ימים',
+    'hour'      => 'שעה|{2} שעתיים|:count שעות',
+    'minute'    => 'דקה|:count דקות',
+    'second'    => 'שנייה|:count שניות',
     'ago'       => 'לפני :time',
     'from_now'  => 'בעוד :time',
     'after'     => ':time אחרי',
