9f2d490631b1426687dc8343593a2d6b04a6f75a
dani3l <dani3l@gmail.com>
2015-05-31 12:07:51 +0300

    Add hebrew locale

---
+++ src/Carbon/Lang/he.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+ 
+/**
+ * Hebrew translation by sUP <dani3l@gmail.com>
+ */
+return array(
+    'year'      => '{0} :count שנים|{1} שנה|{2} שנתיים|[3,Inf] :count שנים',
+    'month'     => '{0} :count חודשים|{1} חודש|{2} חודשיים|[3,Inf] :count חודשים',
+    'week'      => '{0} :count שבועות|{1} שבוע|{2} שבועיים|[3,Inf] :count שבועות',
+    'day'       => '{0} :count ימים|{1} יום|{2} יומיים|[3,Inf] :count ימים',
+    'hour'      => '{0} :count שעות|{1} שעה|{2} שעתיים|[3,Inf] :count שעות',
+    'minute'    => '{0} :count דקות|{1} דקה|[2,Inf] :count דקות',
+    'second'    => '{0} :count שניות|{1} שנייה|[2,Inf] :count שניות',
+    'ago'       => 'לפני :time',
+    'from_now'  => 'בעוד :time',
+    'after'     => ':time אחרי',
+    'before'    => ':time לפני',
+);
