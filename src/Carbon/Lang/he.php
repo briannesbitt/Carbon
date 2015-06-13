6ace47a65f5a6f545ac512064a7ff28add1d8a47
Tuna Abutbul <tuna.abutbul@gmail.com>
2015-06-13 16:27:00 +0300

    add hebrew translation

---
+++ src/Carbon/Lang/he.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+return array(
+    'year'      => 'שנה|:count שנים',
+    'month'     => 'חודש|:count חודשיים',
+    'week'      => 'שבוע|:count שבועות',
+    'day'       => 'יום|:count ימים',
+    'hour'      => 'שעה|:count שעות',
+    'minute'    => 'דקה|:count דקות',
+    'second'    => 'שניה|:count שניות',
+    'ago'       => 'לפני :time',
+    'from_now'  => ':time מעכשיו',
+    'after'     => 'אחרי :time',
+    'before'    => 'לפני :time',
+);
