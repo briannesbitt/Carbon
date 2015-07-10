fad1c31a119a96052397967634edfc7991eb359a
Fadion Dashi <jonidashi@gmail.com>
2015-07-10 21:48:37 +0200

    Add Albanian (sq) locale

---
+++ src/Carbon/Lang/sq.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ *
+ */
+return array(
+    'year'      => '1 vit|:count vjet',
+    'month'     => '1 muaj|:count muaj',
+    'week'      => '1 javë|:count javë',
+    'day'       => '1 ditë|:count ditë',
+    'hour'      => '1 orë|:count orë',
+    'minute'    => '1 minutë|:count minuta',
+    'second'    => '1 sekondë|:count sekonda',
+    'ago'       => ':time më parë',
+    'from_now'  => ':time nga tani',
+    'after'     => ':time pas',
+    'before'    => ':time para',
+);
