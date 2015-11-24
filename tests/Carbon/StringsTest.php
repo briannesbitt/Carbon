dd2af12868ad821ed210ed7b15923fa30756b36b
Lucas Michot <lucas@semalead.com>
2015-11-24 11:43:08 +0100

    Reformat comments

--- tests/Carbon/StringsTest.php
+++ tests/Carbon/StringsTest.php
@@ -58,19 +58,16 @@ class StringsTest extends AbstractTestCase
 
     public function testToLocalizedFormattedDateString()
     {
-        /****************
-
-      Working out a Travis issue on how to set a different locale
-      other than EN to test this.
-
-
-      $cache = setlocale(LC_TIME, 0);
-      setlocale(LC_TIME, 'German');
-      $d = Carbon::create(1975, 12, 25, 14, 15, 16);
-      $this->assertSame('Donnerstag 25 Dezember 1975', $d->formatLocalized('%A %d %B %Y'));
-      setlocale(LC_TIME, $cache);
-
-      *****************/
+        /*
+         * Working out a Travis issue on how to set a different locale
+         * other than EN to test this.
+         *
+         * $cache = setlocale(LC_TIME, 0);
+         * setlocale(LC_TIME, 'German');
+         * $d = Carbon::create(1975, 12, 25, 14, 15, 16);
+         * $this->assertSame('Donnerstag 25 Dezember 1975', $d->formatLocalized('%A %d %B %Y'));
+         * setlocale(LC_TIME, $cache);
+         */
     }
 
     public function testToLocalizedFormattedTimezonedDateString()
