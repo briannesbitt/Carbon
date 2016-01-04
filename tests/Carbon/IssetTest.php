4eccbedea403796d8b4eb87064681d233b18e31c
Lucas Michot <lucas@semalead.com>
2016-01-04 13:17:23 +0100

    Extract properties to a dataProvider

--- tests/Carbon/IssetTest.php
+++ tests/Carbon/IssetTest.php
@@ -21,37 +21,43 @@ class IssetTest extends AbstractTestCase
         $this->assertFalse(isset(Carbon::create(1234, 5, 6, 7, 8, 9)->sdfsdfss));
     }
 
-    public function testIssetReturnTrueForProperties()
+    public function providerTestIssetReturnTrueForProperties()
     {
-        $properties = array(
-            'age',
-            'day',
-            'dayOfWeek',
-            'dayOfYear',
-            'daysInMonth',
-            'dst',
-            'hour',
-            'local',
-            'micro',
-            'minute',
-            'month',
-            'offset',
-            'offsetHours',
-            'quarter',
-            'second',
-            'timestamp',
-            'timezone',
-            'timezoneName',
-            'tz',
-            'tzName',
-            'utc',
-            'weekOfMonth',
-            'weekOfYear',
-            'year',
+        return array(
+            array('age'),
+            array('day'),
+            array('dayOfWeek'),
+            array('dayOfYear'),
+            array('daysInMonth'),
+            array('dst'),
+            array('hour'),
+            array('local'),
+            array('micro'),
+            array('minute'),
+            array('month'),
+            array('offset'),
+            array('offsetHours'),
+            array('quarter'),
+            array('second'),
+            array('timestamp'),
+            array('timezone'),
+            array('timezoneName'),
+            array('tz'),
+            array('tzName'),
+            array('utc'),
+            array('weekOfMonth'),
+            array('weekOfYear'),
+            array('year'),
         );
+    }
 
-        foreach ($properties as $property) {
-            $this->assertTrue(isset(Carbon::create(1234, 5, 6, 7, 8, 9)->$property));
-        }
+    /**
+     * @dataProvider \Tests\Carbon\IssetTest::providerTestIssetReturnTrueForProperties
+     *
+     * @param string $property
+     */
+    public function testIssetReturnTrueForProperties($property)
+    {
+        $this->assertTrue(isset(Carbon::now()->$property, $property));
     }
 }
