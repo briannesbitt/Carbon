c253a19fa1e50fa7aabd3dc55581207036f32dc4
Amr Samy <asamy@data-stars.com>
2016-01-08 02:38:51 +0200

    Remove @covers tag

--- tests/Carbon/AgeTest.php
+++ tests/Carbon/AgeTest.php
@@ -23,18 +23,14 @@ class AgeTest extends AbstractTestCase
      */
     private $testDate = '1984/03/09';
 
-    /**
-     * @covers Carbon::getBirthYear
-     */
+    
     public function testGetBirthYear()
     {
         $age = 32;
         $this->assertEquals(Carbon::now()->subYears($age)->year, Carbon::getBirthYear($age));
     }
 
-    /*
-     * @covers Carbon::getAge
-     */
+
     public function testGetAge()
     {
         $ageArray = Carbon::getAge($this->testDate);
@@ -46,35 +42,27 @@ class AgeTest extends AbstractTestCase
         $this->assertArrayHasKey('second', $ageArray);
     }
 
-    /*
-     * @covers Carbon::getAgeForHumans
-     */
+
     public function testGetAgeForHumans()
     {
         $this->assertStringMatchesFormat('%d year%S,%d month%S,%d day%S,%d hour%S,%d minute%S,%d second%S', Carbon::getAgeForHumans($this->testDate));
     }
 
-    /*
-     * @covers Carbon::getAgeInDays
-     */
+
     public function testGetAgeInDays()
     {
         $date = Carbon::parse($this->testDate);
         $this->assertEquals(Carbon::now()->diffInDays($date), Carbon::getAgeInDays($this->testDate));
     }
 
-    /*
-     * @covers Carbon::getAgeInHours
-     */
+
     public function testGetAgeInHours()
     {
         $date = Carbon::parse($this->testDate);
         $this->assertEquals(Carbon::now()->diffInHours($date), Carbon::getAgeInHours($this->testDate));
     }
 
-    /**
-     * @covers Carbon::getAgeInMinutes
-     */
+
     public function testGetAgeInMinutes()
     {
         $date = Carbon::parse($this->testDate);
