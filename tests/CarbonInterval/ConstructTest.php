eda6465a686ce3afca46fd692200d165f456788b
Amr Samy <asamy@data-stars.com>
2016-01-08 13:58:03 +0200

    Remove white space

--- tests/CarbonInterval/ConstructTest.php
+++ tests/CarbonInterval/ConstructTest.php
@@ -235,7 +235,7 @@ class ConstructTest extends AbstractTestCase
     {
         $ci = CarbonInterval::instance(Carbon::now()->diff(Carbon::now()->addWeeks(3)));
     }
-    
+
     public function testUnknownMethod()
     {
         $this->assertNull(CarbonInterval::notExistMethod());
