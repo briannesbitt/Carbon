430bd5ff2760caa4bbd90f7b75a78c29394b9a3f
balping <balping314@gmail.com>
2016-01-15 22:13:17 +0100

    Hungarian translation improved A LOT
    ... after I realised that actually I can use second_before, second_ago, etc keys.
    Reading the original source code can be really useful!
    
    Also, unit test added for Hungarian language

--- src/Carbon/Lang/hu.php
+++ src/Carbon/Lang/hu.php
@@ -17,15 +17,40 @@
  * Extracted from https://github.com/jenssegers/laravel-date/blob/master/src/lang/hu/date.php
  */
 return array(
-    'year'      => '1 évvel|:count évvel',
-    'month'     => '1 hónappal|:count hónappal',
-    'week'      => '1 héttel|:count héttel',
-    'day'       => '1 nappal|:count nappal',
-    'hour'      => '1 órával|:count órával',
-    'minute'    => '1 perccel|:count perccel',
-    'second'    => '1 másodperccel|:count másodperccel',
-    'ago'       => ':time korábban',
-    'from_now'  => ':time később',
+    'year'      => ':count év',
+    'month'     => ':count hónap',
+    'week'      => ':count hét',
+    'day'       => ':count nap',
+    'hour'      => ':count óra',
+    'minute'    => ':count perc',
+    'second'    => ':count másodperc',
+    'ago'       => ':time',
+    'from_now'  => ':time múlva',
     'after'     => ':time később',
-    'before'    => ':time korábban'
+    'before'    => ':time korábban',
+
+    'year_ago'      => ':count éve',
+    'month_ago'     => ':count hónapja',
+    'week_ago'      => ':count hete',
+    'day_ago'       => ':count napja',
+    'hour_ago'      => ':count órája',
+    'minute_ago'    => ':count perce',
+    'second_ago'    => ':count másodperce',
+
+    'year_after'      => ':count évvel',
+    'month_after'     => ':count hónappal',
+    'week_after'      => ':count héttel',
+    'day_after'       => ':count nappal',
+    'hour_after'      => ':count órával',
+    'minute_after'    => ':count perccel',
+    'second_after'    => ':count másodperccel',
+
+    'year_before'      => ':count évvel',
+    'month_before'     => ':count hónappal',
+    'week_before'      => ':count héttel',
+    'day_before'       => ':count nappal',
+    'hour_before'      => ':count órával',
+    'minute_before'    => ':count perccel',
+    'second_before'    => ':count másodperccel',
+
 );
