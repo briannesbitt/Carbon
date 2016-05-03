27772cd8e89c55a5c54076d8bf6786007511b0b8
Ning Luo <947703573@qq.com>
2016-05-03 16:05:13 +0800

    Fixed CS.

--- tests/Localization/ZhTest.php
+++ tests/Localization/ZhTest.php
@@ -22,7 +22,6 @@ class ZhTest extends AbstractTestCase
 
         $scope = $this;
         $this->wrapWithTestNow(function () use ($scope) {
-        	
             $d = Carbon::now()->subSecond();
             $scope->assertSame('1秒前', $d->diffForHumans());
 
