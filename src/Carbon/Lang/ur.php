7e53f72fd0da4ed87ee578e3c3c6121662ec1490
Mehshan <m.mustafa@mehshan.com>
2016-05-24 22:00:56 +0500

    Added support for Urdu.

---
+++ src/Carbon/Lang/ur.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ */
+
+/**
+ * Contributed by Mehshan Mustafa <m.mustafa@mehshan.com>
+ */
+
+return array(
+    'year'      => ':count سال',
+    'month'     => ':count ماه',
+    'week'      => ':count ہفتے',
+    'day'       => ':count روز',
+    'hour'      => ':count گھنٹے',
+    'minute'    => ':count منٹ',
+    'second'    => ':count سیکنڈ',
+    'ago'       => ':time پہلے',
+    'from_now'  => ':time بعد',
+    'after'     => ':time بعد',
+    'before'    => ':time پہلے',
+);
