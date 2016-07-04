da72f868aaacd851c0df8ac9bd5477ff1be8f494
Mantas Paulauskas <mp@interactivesolutions.lt>
2016-07-04 17:33:06 +0300

    changed array style

--- src/Carbon/Lang/lt.php
+++ src/Carbon/Lang/lt.php
@@ -14,16 +14,16 @@
  * for possible formats.
  */
 
-return [
-    'year'     => ':count metus|:count metus|:count metų',
-    'month'    => '1 mėnesį|:count mėnesius|:count mėnesių',
-    'week'     => '1 savaitę|:count savaites|:count savaičių',
-    'day'      => '1 dieną|:count dienas|:count dienų',
-    'hour'     => ':count valandą|:count valandas|:count valandų',
-    'minute'   => ':count minutę|:count minutes|:count minučių',
-    'second'   => ':count sekundę|:count sekundes|:count sekundžių',
-    'ago'      => 'prieš :time',
-    'from_now' => 'už :time',
-    'after'    => 'po :time',
-    'before'   => ':time nuo dabar',
-];
+return array(
+    'year'      => ':count metus|:count metus|:count metų',
+    'month'     => '1 mėnesį|:count mėnesius|:count mėnesių',
+    'week'      => '1 savaitę|:count savaites|:count savaičių',
+    'day'       => '1 dieną|:count dienas|:count dienų',
+    'hour'      => ':count valandą|:count valandas|:count valandų',
+    'minute'    => ':count minutę|:count minutes|:count minučių',
+    'second'    => ':count sekundę|:count sekundes|:count sekundžių',
+    'ago'       => 'prieš :time',
+    'from_now'  => 'už :time',
+    'after'     => 'po :time',
+    'before'    => ':time nuo dabar',
+);
