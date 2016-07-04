d745d41dd7c9fcb36e12feb971cf697b735187ac
Mantas <mantopastas@gmail.com>
2016-07-04 17:02:46 +0300

    Added tests
    Added tests

--- tests/Localization/LtTest.php
+++ tests/Localization/LtTest.php
@@ -31,17 +31,29 @@ class LtTest extends AbstractTestCase
             $d = Carbon::now()->subSeconds(21);
             $scope->assertSame('prieš 21 sekundę', $d->diffForHumans());
 
+            $d = Carbon::now()->subSeconds(40);
+            $scope->assertSame('prieš 40 sekundžių', $d->diffForHumans());
+
             $d = Carbon::now()->subMinute();
             $scope->assertSame('prieš 1 minutę', $d->diffForHumans());
 
             $d = Carbon::now()->subMinutes(2);
             $scope->assertSame('prieš 2 minutes', $d->diffForHumans());
 
+            $d = Carbon::now()->subMinutes(21);
+            $scope->assertSame('prieš 21 minutę', $d->diffForHumans());
+
             $d = Carbon::now()->subHour();
             $scope->assertSame('prieš 1 valandą', $d->diffForHumans());
 
             $d = Carbon::now()->subHours(2);
             $scope->assertSame('prieš 2 valandas', $d->diffForHumans());
+
+            $d = Carbon::now()->subHours(20);
+            $scope->assertSame('prieš 20 valandų', $d->diffForHumans());
+
+            $d = Carbon::now()->subHours(21);
+            $scope->assertSame('prieš 21 valandą', $d->diffForHumans());
 
             $d = Carbon::now()->subDay();
             $scope->assertSame('prieš 1 dieną', $d->diffForHumans());
@@ -61,14 +73,23 @@ class LtTest extends AbstractTestCase
             $d = Carbon::now()->subMonths(2);
             $scope->assertSame('prieš 2 mėnesius', $d->diffForHumans());
 
+            $d = Carbon::now()->subMonths(11);
+            $scope->assertSame('prieš 11 mėnesių', $d->diffForHumans());
+
             $d = Carbon::now()->subYear();
             $scope->assertSame('prieš 1 metus', $d->diffForHumans());
 
             $d = Carbon::now()->subYears(2);
             $scope->assertSame('prieš 2 metus', $d->diffForHumans());
 
+            $d = Carbon::now()->subYears(10);
+            $scope->assertSame('prieš 10 metų', $d->diffForHumans());
+
+            $d = Carbon::now()->subYears(21);
+            $scope->assertSame('prieš 21 metus', $d->diffForHumans());
+
             $d = Carbon::now()->addSecond();
-            $scope->assertSame('už 1 sekundės', $d->diffForHumans());
+            $scope->assertSame('už 1 sekundę', $d->diffForHumans());
         });
     }
 }
