00879c507ab9d173cc13d54187e279d9ceb62775
Justinas Bolys <justinas.bolys@nfq.com>
2016-07-09 13:04:12 +0300

    fixed lithuanian tests and added more translations.

--- tests/Localization/LtTest.php
+++ tests/Localization/LtTest.php
@@ -43,6 +43,9 @@ class LtTest extends AbstractTestCase
             $d = Carbon::now()->subHours(2);
             $scope->assertSame('prieš 2 valandas', $d->diffForHumans());
 
+            $d = Carbon::now()->subHours(21);
+            $scope->assertSame('prieš 21 valandą', $d->diffForHumans());
+
             $d = Carbon::now()->subDay();
             $scope->assertSame('prieš 1 dieną', $d->diffForHumans());
 
@@ -69,6 +72,58 @@ class LtTest extends AbstractTestCase
 
             $d = Carbon::now()->addSecond();
             $scope->assertSame('už 1 sekundės', $d->diffForHumans());
+
+            $d = Carbon::now()->addSeconds(21);
+            $scope->assertSame('už 21 sekundės', $d->diffForHumans());
+
+            $d = Carbon::now()->addSeconds(15);
+            $scope->assertSame('už 15 sekundžių', $d->diffForHumans());
+
+            $d = Carbon::now()->addMinute();
+            $scope->assertSame('už 1 minutės', $d->diffForHumans());
+
+            $d = Carbon::now()->addMinutes(15);
+            $scope->assertSame('už 15 minučių', $d->diffForHumans());
+
+            $d = Carbon::now()->addMinutes(21);
+            $scope->assertSame('už 21 minutės', $d->diffForHumans());
+
+            $d = Carbon::now()->addHour();
+            $scope->assertSame('už 1 valandos', $d->diffForHumans());
+
+            $d = Carbon::now()->addHours(15);
+            $scope->assertSame('už 15 valandų', $d->diffForHumans());
+
+            $d = Carbon::now()->addHours(21);
+            $scope->assertSame('už 21 valandos', $d->diffForHumans());
+
+            $d = Carbon::now()->addDay();
+            $scope->assertSame('už 1 dienos', $d->diffForHumans());
+
+            $d = Carbon::now()->addDays(2);
+            $scope->assertSame('už 2 dienų', $d->diffForHumans());
+
+            $d = Carbon::now()->addWeek();
+            $scope->assertSame('už 1 savaitės', $d->diffForHumans());
+
+            $d = Carbon::now()->addWeek(2);
+            $scope->assertSame('už 2 savaičių', $d->diffForHumans());
+
+            $d = Carbon::now()->addMonth();
+            $scope->assertSame('už 1 mėnesio', $d->diffForHumans());
+
+            $d = Carbon::now()->addMonths(2);
+            $scope->assertSame('už 2 mėnesių', $d->diffForHumans());
+
+            $d = Carbon::now()->addMonths(11);
+            $scope->assertSame('už 11 mėnesių', $d->diffForHumans());
+
+            $d = Carbon::now()->addYear();
+            $scope->assertSame('už 1 metų', $d->diffForHumans());
+
+            $d = Carbon::now()->addYears(10);
+            $scope->assertSame('už 10 metų', $d->diffForHumans());
+
         });
     }
 }
