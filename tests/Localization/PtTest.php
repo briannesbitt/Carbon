b6da75fe721ca6b10e005ec3456069b7689f6b7a
swalker <swalker2@users.noreply.github.com>
2016-07-15 16:31:25 -0300

    Tests For pt-BR diffForHumans

--- tests/Localization/PtTest.php
+++ tests/Localization/PtTest.php
@@ -35,6 +35,48 @@ class PtTest extends AbstractTestCase
         $this->wrapWithTestNow(function () use ($scope) {
             $d = Carbon::now()->subSecond();
             $scope->assertSame('há 1 segundo', $d->diffForHumans());
+    
+            $d = Carbon::now()->subSeconds(2);
+            $scope->assertSame('há 2 segundos', $d->diffForHumans());
+    
+            $d = Carbon::now()->subMinute();
+            $scope->assertSame('há 1 minuto', $d->diffForHumans());
+    
+            $d = Carbon::now()->subMinutes(2);
+            $scope->assertSame('há 2 minutos', $d->diffForHumans());
+    
+            $d = Carbon::now()->subHour();
+            $scope->assertSame('há 1 hora', $d->diffForHumans());
+    
+            $d = Carbon::now()->subHours(2);
+            $scope->assertSame('há 2 horas', $d->diffForHumans());
+    
+            $d = Carbon::now()->subDay();
+            $scope->assertSame('há 1 dia', $d->diffForHumans());
+    
+            $d = Carbon::now()->subDays(2);
+            $scope->assertSame('há 2 dias', $d->diffForHumans());
+    
+            $d = Carbon::now()->subWeek();
+            $scope->assertSame('há 1 semana', $d->diffForHumans());
+    
+            $d = Carbon::now()->subWeeks(2);
+            $scope->assertSame('há 2 semanas', $d->diffForHumans());
+    
+            $d = Carbon::now()->subMonth();
+            $scope->assertSame('há 1 mês', $d->diffForHumans());
+    
+            $d = Carbon::now()->subMonths(2);
+            $scope->assertSame('há 2 meses', $d->diffForHumans());
+    
+            $d = Carbon::now()->subYear();
+            $scope->assertSame('há 1 ano', $d->diffForHumans());
+    
+            $d = Carbon::now()->subYears(2);
+            $scope->assertSame('há 2 anos', $d->diffForHumans());
+    
+            $d = Carbon::now()->addSecond();
+            $scope->assertSame('em 1 segundo', $d->diffForHumans());
         });
     }
 
