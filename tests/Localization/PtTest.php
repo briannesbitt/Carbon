9bedee5c912ff39ecc0a6534b8b19ae643ce12f6
Brian Nesbitt <brian@nesbot.com>
2016-07-21 15:29:28 -0400

    Applied fixes from StyleCI

--- tests/Localization/PtTest.php
+++ tests/Localization/PtTest.php
@@ -26,51 +26,51 @@ class PtTest extends AbstractTestCase
 
             $d = Carbon::now()->subSeconds(2);
             $scope->assertSame('2 segundos atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMinute();
             $scope->assertSame('1 minuto atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMinutes(2);
             $scope->assertSame('2 minutos atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subHour();
             $scope->assertSame('1 hora atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subHours(2);
             $scope->assertSame('2 horas atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subDay();
             $scope->assertSame('1 dia atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subDays(2);
             $scope->assertSame('2 dias atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subWeek();
             $scope->assertSame('1 semana atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subWeeks(2);
             $scope->assertSame('2 semanas atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMonth();
             $scope->assertSame('1 mês atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMonths(2);
             $scope->assertSame('2 meses atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subYear();
             $scope->assertSame('1 ano atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subYears(2);
             $scope->assertSame('2 anos atrás', $d->diffForHumans());
-            
+
             $d = Carbon::now()->addSecond();
             $scope->assertSame('em 1 segundo', $d->diffForHumans());
-            
+
             $d = Carbon::now()->addSecond();
             $d2 = Carbon::now();
             $scope->assertSame('1 segundo depois', $d->diffForHumans($d2));
             $scope->assertSame('1 segundo antes', $d2->diffForHumans($d));
-            
+
             $scope->assertSame('1 segundo', $d->diffForHumans($d2, true));
             $scope->assertSame('2 segundos', $d2->diffForHumans($d->addSecond(), true));
         });
@@ -86,51 +86,51 @@ class PtTest extends AbstractTestCase
 
             $d = Carbon::now()->subSeconds(2);
             $scope->assertSame('há 2 segundos', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMinute();
             $scope->assertSame('há 1 minuto', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMinutes(2);
             $scope->assertSame('há 2 minutos', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subHour();
             $scope->assertSame('há 1 hora', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subHours(2);
             $scope->assertSame('há 2 horas', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subDay();
             $scope->assertSame('há 1 dia', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subDays(2);
             $scope->assertSame('há 2 dias', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subWeek();
             $scope->assertSame('há 1 semana', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subWeeks(2);
             $scope->assertSame('há 2 semanas', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMonth();
             $scope->assertSame('há 1 mês', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMonths(2);
             $scope->assertSame('há 2 meses', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subYear();
             $scope->assertSame('há 1 ano', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subYears(2);
             $scope->assertSame('há 2 anos', $d->diffForHumans());
-            
+
             $d = Carbon::now()->addSecond();
             $scope->assertSame('em 1 segundo', $d->diffForHumans());
-            
+
             $d = Carbon::now()->addSecond();
             $d2 = Carbon::now();
             $scope->assertSame('após 1 segundo', $d->diffForHumans($d2));
             $scope->assertSame('1 segundo atrás', $d2->diffForHumans($d));
-            
+
             $scope->assertSame('1 segundo', $d->diffForHumans($d2, true));
             $scope->assertSame('2 segundos', $d2->diffForHumans($d->addSecond(), true));
         });
@@ -146,51 +146,51 @@ class PtTest extends AbstractTestCase
 
             $d = Carbon::now()->subSeconds(2);
             $scope->assertSame('há 2 segundos', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMinute();
             $scope->assertSame('há 1 minuto', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMinutes(2);
             $scope->assertSame('há 2 minutos', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subHour();
             $scope->assertSame('há 1 hora', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subHours(2);
             $scope->assertSame('há 2 horas', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subDay();
             $scope->assertSame('há 1 dia', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subDays(2);
             $scope->assertSame('há 2 dias', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subWeek();
             $scope->assertSame('há 1 semana', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subWeeks(2);
             $scope->assertSame('há 2 semanas', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMonth();
             $scope->assertSame('há 1 mês', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subMonths(2);
             $scope->assertSame('há 2 meses', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subYear();
             $scope->assertSame('há 1 ano', $d->diffForHumans());
-            
+
             $d = Carbon::now()->subYears(2);
             $scope->assertSame('há 2 anos', $d->diffForHumans());
-            
+
             $d = Carbon::now()->addSecond();
             $scope->assertSame('em 1 segundo', $d->diffForHumans());
-            
+
             $d = Carbon::now()->addSecond();
             $d2 = Carbon::now();
             $scope->assertSame('após 1 segundo', $d->diffForHumans($d2));
             $scope->assertSame('1 segundo atrás', $d2->diffForHumans($d));
-            
+
             $scope->assertSame('1 segundo', $d->diffForHumans($d2, true));
             $scope->assertSame('2 segundos', $d2->diffForHumans($d->addSecond(), true));
         });
