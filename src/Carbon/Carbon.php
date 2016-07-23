a3e18dba2dc268ab990e3d864b877499a8ec49fe
Lucas Michot <lucas@semalead.com>
2016-07-23 14:20:48 +0200

    Factorize previous- and next- week- or weekend day

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -2977,17 +2977,30 @@ class Carbon extends DateTime
     }
 
     /**
+     * Go forward or backward to the next week- or weekend-day.
+     *
+     * @param bool $weekday
+     * @param bool $forward
+     *
+     * @return $this
+     */
+    private function nextOrPreviousDay($weekday = true, $forward = true)
+    {
+        do {
+            $this->addDay($forward ? 1 : - 1);
+        } while ($weekday ? $this->isWeekend() : $this->isWeekday());
+
+        return $this;
+    }
+
+    /**
      * Go forward to the next weekday.
      *
      * @return $this
      */
     public function nextWeekday()
     {
-        do {
-            $this->addDay();
-        } while ($this->isWeekend());
-
-        return $this;
+        return $this->nextOrPreviousDay();
     }
 
     /**
@@ -2997,11 +3010,7 @@ class Carbon extends DateTime
      */
     public function previousWeekday()
     {
-        do {
-            $this->subDay();
-        } while ($this->isWeekend());
-
-        return $this;
+        return $this->nextOrPreviousDay(true, false);
     }
 
     /**
@@ -3011,11 +3020,7 @@ class Carbon extends DateTime
      */
     public function nextWeekendDay()
     {
-        do {
-            $this->addDay();
-        } while ($this->isWeekday());
-
-        return $this;
+        return $this->nextOrPreviousDay(false);
     }
 
     /**
@@ -3025,11 +3030,7 @@ class Carbon extends DateTime
      */
     public function previousWeekendDay()
     {
-        do {
-            $this->subDay();
-        } while ($this->isWeekday());
-
-        return $this;
+        return $this->nextOrPreviousDay(false, false);
     }
 
     /**
