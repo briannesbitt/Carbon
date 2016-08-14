1e41b0ca8169d53d9091d5b6d097d930230398cc
Alex <alexandr.cctvgmail.com>
2016-08-14 15:28:04 +0300

    Add Ukraine language

---
+++ src/Carbon/Lang/ua.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Translation messages.  See http://symfony.com/doc/current/book/translation.html
+ * for possible formats.
+ */
+/**
+ *  Translated Greenelf https://github.com/Greenelf/
+ */
+return array(
+    'year'      => ':count рік|:count року|:count років',
+    'month'     => ':count місяць|:count місяця|:count місяців',
+    'week'      => ':count неділю|:count неділі|:count неділь',
+    'day'       => ':count днів|:count дня|:count днів',
+    'hour'      => ':count година|:count години|:count годин',
+    'minute'    => ':count хвилину|:count хвилини|:count хвилин',
+    'second'    => ':count секунду|:count секунди|:count секунд',
+    'ago'       => ':time назад',
+    'from_now'  => 'через :time',
+    'after'     => ':time після',
+    'before'    => ':time до'
+);
