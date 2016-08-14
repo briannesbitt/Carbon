50ec421e316530a7212291cf01fef91990358dd9
Greenelf <alexandr.cctv@gmail.com>
2016-08-14 15:33:47 +0300

    Update ua.php

--- src/Carbon/Lang/ua.php
+++ src/Carbon/Lang/ua.php
@@ -1,18 +1,7 @@
 <?php
-/*
- * This file is part of the Carbon package.
- *
- * (c) Brian Nesbitt <brian@nesbot.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
- 
 /**
  * Translation messages.  See http://symfony.com/doc/current/book/translation.html
  * for possible formats.
- */
-/**
  *  Translated Greenelf https://github.com/Greenelf/
  */
 return array(
