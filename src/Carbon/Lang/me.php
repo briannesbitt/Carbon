3870c6ad218ad46be4c0725b9ac30dae36598cc9
Milos <milose@users.noreply.github.com>
2016-11-25 16:53:11 +0100

    Add Montenegrin locale

---
+++ src/Carbon/Lang/me.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return array(
+    'year' => ':count godinu|:count godine|:count godina',
+    'y' => ':count godinu|:count godine|:count godina',
+    'month' => ':count mjesec|:count mjeseca|:count mjeseci',
+    'm' => ':count mjesec|:count mjeseca|:count mjeseci',
+    'week' => ':count sedmica|:count sedmice|:count sedmica',
+    'w' => ':count sedmica|:count sedmice|:count sedmica',
+    'day' => ':count dan|:count dana|:count dana',
+    'd' => ':count dan|:count dana|:count dana',
+    'hour' => ':count sat|:count sata|:count sati',
+    'h' => ':count sat|:count sata|:count sati',
+    'minute' => ':count minut|:count minuta |:count minuta',
+    'min' => ':count minut|:count minuta |:count minuta',
+    'second' => ':count sekund|:count sekunde|:count sekundi',
+    's' => ':count sekund|:count sekunde|:count sekundi',
+    'ago' => 'prije :time',
+    'from_now' => 'za :time',
+    'after' => 'za :time',
+    'before' => 'prije :time',
+);
