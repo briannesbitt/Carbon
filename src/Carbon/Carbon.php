c6bd35e1f92bdc249ff4cd28718169817b492e61
Robin D'Arcy <robin@klever.co.uk>
2017-05-18 14:35:40 +0100

    Add __set_state() method

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -3367,4 +3367,16 @@ class Carbon extends DateTime
 
         return $instance;
     }
+
+    /**
+     * The __set_state handler.
+     *
+     * @param array $array
+     *
+     * @return static
+     */
+    public static function __set_state($array)
+    {
+        return static::instance(parent::__set_state($array));
+    }
 }
