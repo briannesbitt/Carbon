09d631dd8ac74d2b2c2d17fa3d8863a0aeea57e3
or <igor_deyawka@mail.ru>
2018-01-15 19:31:24 +0200

    Fixed issue #883: reverted pull-request #548 about issue #88
    The realisation of initial issue #88 is wrong. The problem does not relies to `local` / `not local` timezone. Php handles date changes similar for both cases. So in fact changes was made 'fixes' non-local dates but local dates works same as before.
    And it 'fixes' the problem and not actually fixes it because in fact the PHP handling of DST changing in non-UTC dates is quite right. If developer don't want to adjusting timestamp to DST change he should use UTC timezone.
    
    The unit tests added to assure default php behavior.
    
    Next commits reverted by hand because conflicts in automatic revert.
    
    * Commit 2cc69881  Fix CS	07.12.2015 13:04	Lucas Michot
    * Commit 5394301d  Fix issue 88	07.12.2015 12:55	Lucas Michot

---
+++ tests/Carbon/ModifyNearDSTChangeTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Carbon;
+
+use Carbon\Carbon;
+use Tests\AbstractTestCase;
+
+class ModifyNearDSTChangeTest extends AbstractTestCase
+{
+    /**
+     * Tests transition through DST change hour in non default timezone
+     *
+     * @dataProvider getTransitionTests
+     */
+    public function testTransitionInNonDefaultTimezone($dateString, $addHours, $expected)
+    {
+        date_default_timezone_set('Europe/london');
+        $date = Carbon::parse($dateString, 'America/New_York');
+        $date->addHours($addHours);
+        $this->assertSame($expected, $date->format('c'));
+    }
+
+    /**
+     * Tests transition through DST change hour in default timezone
+     *
+     * @dataProvider getTransitionTests
+     */
+    public function testTransitionInDefaultTimezone($dateString, $addHours, $expected)
+    {
+        date_default_timezone_set('America/New_York');
+        $date = Carbon::parse($dateString, 'America/New_York');
+        $date->addHours($addHours);
+        $this->assertSame($expected, $date->format('c'));
+    }
+
+    public function getTransitionTests()
+    {
+
+        // arguments:
+        // - Date string to Carbon::parse in America/New_York.
+        // - Hours to add
+        // - Resulting string in 'c' format
+        $tests = array(
+            // testForwardTransition
+            // When standard time was about to reach 2010-03-14T02:00:00-05:00 clocks were turned forward 1 hour to
+            // 2010-03-14T03:00:00-04:00 local daylight time instead
+            array('2010-03-14T00:00:00', 24, '2010-03-15T00:00:00-04:00'),
+
+            // testBackwardTransition
+            // When local daylight time was about to reach 2010-11-07T02:00:00-04:00 clocks were turned backward 1 hour
+            // to 2010-11-07T01:00:00-05:00 local standard time instead
+            array('2010-11-07T00:00:00', 24, '2010-11-08T00:00:00-05:00'),
+        );
+
+        return $tests;
+    }
+}
