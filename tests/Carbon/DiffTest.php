083baa342fb756ff07993dcd6e5aa4832f0b37f2
KyleKatarn <kylekatarnls@gmail.com>
2018-02-20 12:56:02 +0100

    Add a createMidnightDate method

--- tests/Carbon/DiffTest.php
+++ tests/Carbon/DiffTest.php
@@ -407,6 +407,11 @@ class DiffTest extends AbstractTestCase
         $dtVancouver = Carbon::createFromDate(2012, 1, 1, 'America/Vancouver');
 
         $this->assertSame(0, $dtVancouver->diffInHours($dtToronto) % 24);
+
+        $dtToronto = Carbon::createMidnightDate(2012, 1, 1, 'America/Toronto');
+        $dtVancouver = Carbon::createMidnightDate(2012, 1, 1, 'America/Vancouver');
+
+        $this->assertSame(3, $dtVancouver->diffInHours($dtToronto), 'Midnight in Toronto is 3 hours from midnight in Vancouver');
     }
 
     public function testDiffInMinutesPositive()
