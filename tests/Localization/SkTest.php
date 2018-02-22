f20296152c62cd7053e9438137e98a28166cb192
Marek Adamický <marek.adamicky@gmail.com>
2018-02-22 18:37:25 +0100

    Fix SkTest.php (#1117)
    * Fix SkTest.php

--- tests/Localization/SkTest.php
+++ tests/Localization/SkTest.php
@@ -19,6 +19,7 @@ class SkTest extends AbstractTestCase
     public function testDiffForHumansLocalizedInSlovak()
     {
         Carbon::setLocale('sk');
+        Carbon::setUtf8(true);
 
         $scope = $this;
         $this->wrapWithNonDstDate(function () use ($scope) {
@@ -67,11 +68,38 @@ class SkTest extends AbstractTestCase
             $d = Carbon::now()->addSecond();
             $scope->assertSame('za sekundu', $d->diffForHumans());
 
+            $d = Carbon::now()->addSeconds(2);
+            $scope->assertSame('za 2 sekundy', $d->diffForHumans());
+
+            $d = Carbon::now()->addSeconds(5);
+            $scope->assertSame('za 5 sekúnd', $d->diffForHumans());
+
+            $d = Carbon::now()->addYear();
+            $scope->assertSame('za rok', $d->diffForHumans());
+
+            $d = Carbon::now()->addYear(2);
+            $scope->assertSame('za 2 roky', $d->diffForHumans());
+
+            $d = Carbon::now()->addYear(5);
+            $scope->assertSame('za 5 rokov', $d->diffForHumans());
+
             $d = Carbon::now()->addSecond();
             $d2 = Carbon::now();
             $scope->assertSame('o sekundu neskôr', $d->diffForHumans($d2));
             $scope->assertSame('sekundu predtým', $d2->diffForHumans($d));
 
+            $d = Carbon::now()->addSecond(2);
+            $d2 = Carbon::now();
+            $scope->assertSame('o 2 sekundy neskôr', $d->diffForHumans($d2));
+            $scope->assertSame('2 sekundy predtým', $d2->diffForHumans($d));
+
+            $d = Carbon::now()->addSecond(5);
+            $d2 = Carbon::now();
+            $scope->assertSame('o 5 sekúnd neskôr', $d->diffForHumans($d2));
+            $scope->assertSame('5 sekúnd predtým', $d2->diffForHumans($d));
+
+            $d = Carbon::now()->addSecond();
+            $d2 = Carbon::now();
             $scope->assertSame('sekundu', $d->diffForHumans($d2, true));
             $scope->assertSame('2 sekundy', $d2->diffForHumans($d->addSecond(), true));
         });
