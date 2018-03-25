94163a2601f4d1540d804a57ca96b4b6449b85ee
Daniel Monaghan <GLaDOSDan@users.noreply.github.com>
2018-03-25 04:07:38 +0100

    Added Welsh Language support

---
+++ src/Carbon/Lang/cy.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return array(
+    'year' => '1 flwyddyn|:count blynedd',
+    'y' => '1bl|:countbl',
+    'month' => '1 mis|:count fis',
+    'm' => '1mi|:countmi',
+    'week' => ':count wythnos',
+    'w' => '1w|:countw',
+    'day' => '|:count diwrnod',
+    'd' => '1d|:countd',
+    'hour' => ':count awr',
+    'h' => '1h|:counth',
+    'minute' => ':count munud',
+    'min' => '1m|:countm',
+    'second' => ':count eiliad',
+    's' => '1s|:counts',
+    'ago' => ':time yn ôl',
+    'from_now' => ':time o hyn ymlaen',
+    'after' => ':time ar ôl',
+    'before' => ':time o\'r blaen',
+);
