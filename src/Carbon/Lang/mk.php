b5bfaea4454f486d3e80b9f5f214749d09b3d6a3
KyleKatarn <kylekatarnls@gmail.com>
2018-04-13 11:37:23 +0200

    Replace "1" by :count to support other singular values (such as 0 in many languages)

--- src/Carbon/Lang/mk.php
+++ src/Carbon/Lang/mk.php
@@ -10,13 +10,13 @@
  */
 
 return array(
-    'year' => '1 година|:count години',
-    'month' => '1 месец|:count месеци',
-    'week' => '1 седмица|:count седмици',
-    'day' => '1 ден|:count дена',
-    'hour' => '1 час|:count часа',
-    'minute' => '1 минута|:count минути',
-    'second' => '1 секунда|:count секунди',
+    'year' => ':count година|:count години',
+    'month' => ':count месец|:count месеци',
+    'week' => ':count седмица|:count седмици',
+    'day' => ':count ден|:count дена',
+    'hour' => ':count час|:count часа',
+    'minute' => ':count минута|:count минути',
+    'second' => ':count секунда|:count секунди',
     'ago' => 'пред :time',
     'from_now' => ':time од сега',
     'after' => 'по :time',
