bec076d36a64e79028c2de81a53c857f69632184
MegaXLR <admin@megaxlr.net>
2018-05-02 11:08:00 +0200

    Add Diff translations for Dutch (#1273)
    * Add Diff translations for Dutch
    
    There is still one string that I could  not translate,  since that does
    not exist in Dutch.
    
    If anyone else knows the word for "before yesterday" in English, please
    leave a comment on the PR.
    
    "Voorgisteren" would be the closest I can imagine, but its not correct.
    
    Closing briannesbitt/Carbon#1272
    
    Signed-off-by: MegaXLR <admin@megaxlr.net>
    
    * Add correct "2 days before" string in Dutch
    
    After consulting my dictionary, this is the correct string.
    I totally forgot about this word before my last commit.
    
    Signed-off-by: MegaXLR <admin@megaxlr.net>

--- src/Carbon/Lang/nl.php
+++ src/Carbon/Lang/nl.php
@@ -28,4 +28,9 @@ return array(
     'from_now' => 'over :time',
     'after' => ':time later',
     'before' => ':time eerder',
+    'diff_now' => 'nu',
+    'diff_yesterday' => 'gisteren',
+    'diff_tomorrow' => 'morgen',
+    'diff_after_tomorrow' => 'overmorgen',
+    'diff_before_yesterday' => 'eergisteren',
 );
