0722cb734b9024a166fbf21ac11a9a48c9222560
KyleK <kylekatarnls@gmail.com>
2018-05-13 16:25:33 +0200

    Add obsolete serbo-croatian language Translation source: https://github.com/jenssegers/date/blame/1b410d46d1d1efb7f69e568b672b2c64945fc8ce/src/Lang/sh.php

---
+++ src/Carbon/Lang/sh.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return array(
+    'year' => ':count godina|:count godine|:count godina',
+    'y' => ':count godina|:count godine|:count godina',
+    'month' => ':count mesec|:count meseca|:count meseci',
+    'm' => ':count mesec|:count meseca|:count meseci',
+    'week' => ':count nedelja|:count nedelje|:count nedelja',
+    'w' => ':count nedelja|:count nedelje|:count nedelja',
+    'day' => ':count dan|:count dana|:count dana',
+    'd' => ':count dan|:count dana|:count dana',
+    'hour' => ':count čas|:count časa|:count časova',
+    'h' => ':count čas|:count časa|:count časova',
+    'minute' => ':count minut|:count minuta|:count minuta',
+    'min' => ':count minut|:count minuta|:count minuta',
+    'second' => ':count sekund|:count sekunda|:count sekundi',
+    's' => ':count sekund|:count sekunda|:count sekundi',
+    'ago' => 'pre :time',
+    'from_now' => 'za :time',
+    'after' => 'nakon :time',
+    'before' => ':time raniјe',
+);
