b5d00edb0f791aa9a0b6e99301f8bf979145843c
Kyle <kylekatarnls@users.noreply.github.com>
2018-05-18 21:32:40 +0200

    Add unit test for CarbonInterval::copy() and make() (#1309)

--- tests/CarbonInterval/ConstructTest.php
+++ tests/CarbonInterval/ConstructTest.php
@@ -246,6 +246,23 @@ class ConstructTest extends AbstractTestCase
         $this->assertCarbonInterval($ci, 0, 0, 21, 0, 0, 0);
     }
 
+    public function testCopy()
+    {
+        $one = CarbonInterval::days(10);
+        $two = $one->hours(6)->copy()->hours(3);
+        $this->assertCarbonInterval($one, 0, 0, 10, 6, 0, 0);
+        $this->assertCarbonInterval($two, 0, 0, 10, 3, 0, 0);
+    }
+
+    public function testMake()
+    {
+        $this->assertCarbonInterval(CarbonInterval::make('3 hours 30 m'), 0, 0, 0, 3, 30, 0);
+        $this->assertCarbonInterval(CarbonInterval::make('PT5H'), 0, 0, 0, 5, 0, 0);
+        $this->assertCarbonInterval(CarbonInterval::make(new DateInterval('P1D')), 0, 0, 1, 0, 0, 0);
+        $this->assertCarbonInterval(CarbonInterval::make(new CarbonInterval('P2M')), 0, 2, 0, 0, 0, 0);
+        $this->assertNull(CarbonInterval::make(3));
+    }
+
     public function testCallInvalidStaticMethod()
     {
         $this->assertNull(CarbonInterval::anything());
