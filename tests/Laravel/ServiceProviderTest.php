10b682da991f9dc88e7aaba3f0df260808f26a71
KyleKatarn <kylekatarnls@gmail.com>
2018-06-21 10:23:52 +0200

    Automate synchronization with Laravel locale

---
+++ tests/Laravel/ServiceProviderTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Carbon\Laravel;
+
+use Carbon\Carbon;
+use Carbon\Laravel\ServiceProvider;
+use PHPUnit\Framework\TestCase;
+
+class ServiceProviderTest extends TestCase
+{
+    public function testBoot()
+    {
+        include_once __DIR__.'/ServiceProvider.php';
+        $service = new ServiceProvider();
+
+        $this->assertSame('en', Carbon::getLocale());
+        $service->boot();
+        $this->assertSame('de', Carbon::getLocale());
+        $service->app->setLocale('fr');
+        $this->assertSame('fr', Carbon::getLocale());
+    }
+}
