81d7bc2c07c122a8a7e4e53f805b0f3a9ade3416
KyleKatarn <kylekatarnls@gmail.com>
2018-06-28 11:08:05 +0200

    Support of Laravel 5.5

--- src/Carbon/Laravel/ServiceProvider.php
+++ src/Carbon/Laravel/ServiceProvider.php
@@ -4,24 +4,27 @@ namespace Carbon\Laravel;
 
 use Carbon\Carbon;
 use Illuminate\Events\EventDispatcher;
+use Illuminate\Events\Dispatcher;
 use Symfony\Component\Translation\Translator;
+use Illuminate\Translation\Translator as IlluminateTranslator;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function boot()
     {
         $service = $this;
-        if (($events = $this->app['events']) instanceof EventDispatcher) {
+        $events = $this->app['events'];
+        if ($events instanceof EventDispatcher || $events instanceof Dispatcher) {
             $events->listen(version_compare($this->app->version(), '5.5') >= 0 ? 'Illuminate\Foundation\Events\LocaleUpdated' : 'locale.changed', function () use ($service) {
                 $service->updateLocale();
             });
             $service->updateLocale();
         }
     }
-
     public function updateLocale()
     {
-        if (($translator = $this->app['translator']) instanceof Translator) {
+        $translator = $this->app['translator'];
+        if ($translator instanceof Translator || $translator instanceof IlluminateTranslator) {
             Carbon::setLocale($translator->getLocale());
         }
     }
