64563e2b9f69e4db1b82a60e81efa327a30ff343
Max Kovpak <max_kovpak@hotmail.com>
2018-07-05 09:59:26 +0300

    Wrong day of a week exception (#1382)
    If set start of a week more than 6 or less than 0 and then call startOfWeek we will get an infinite while loop... So, I think it is a good idea to throw Exception when a user sets start/end of a week.

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -1447,10 +1447,16 @@ class Carbon extends DateTime implements JsonSerializable
      *
      * @param int $day week start day
      *
+     * @throws InvalidArgumentException
+     *
      * @return void
      */
     public static function setWeekStartsAt($day)
     {
+        if ($day > static::SATURDAY || $day < static::SUNDAY) {
+            throw new InvalidArgumentException('Day of a week should be greater than or equal to 0 and less than or equal to 6.');
+        }
+
         static::$weekStartsAt = $day;
     }
 
@@ -1469,10 +1475,16 @@ class Carbon extends DateTime implements JsonSerializable
      *
      * @param int $day
      *
+     * @throws InvalidArgumentException
+     *
      * @return void
      */
     public static function setWeekEndsAt($day)
     {
+        if ($day > static::SATURDAY || $day < static::SUNDAY) {
+            throw new InvalidArgumentException('Day of a week should be greater than or equal to 0 and less than or equal to 6.');
+        }
+
         static::$weekEndsAt = $day;
     }
 
