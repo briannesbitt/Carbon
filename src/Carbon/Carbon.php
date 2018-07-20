bfb14b314839b50e2f8f18b6dad3415f92ec9f2d
Jordan Hoff <jhoff@linkedin.com>
2018-07-20 12:39:21 -0500

    Fix docblock param type

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -1847,7 +1847,7 @@ class Carbon extends DateTime implements JsonSerializable
     /**
      * Set the default format used when type juggling a Carbon instance to a string
      *
-     * @param mixed $format
+     * @param string|Closure $format
      *
      * @return void
      */
