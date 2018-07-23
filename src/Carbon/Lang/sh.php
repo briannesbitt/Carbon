5aaa06844f875667a062bec01252355785f59c16
Kyle <kylekatarnls@users.noreply.github.com>
2018-07-23 15:34:35 +0200

    Fix serbo-croatian plural (#1401)

--- src/Carbon/Lang/sh.php
+++ src/Carbon/Lang/sh.php
@@ -9,6 +9,10 @@
  * file that was distributed with this source code.
  */
 
+\Symfony\Component\Translation\PluralizationRules::set(function ($number) {
+    return ((1 == $number % 10) && (11 != $number % 100)) ? 0 : ((($number % 10 >= 2) && ($number % 10 <= 4) && (($number % 100 < 10) || ($number % 100 >= 20))) ? 1 : 2);
+}, 'sh');
+
 return array(
     'year' => ':count godina|:count godine|:count godina',
     'y' => ':count godina|:count godine|:count godina',
--- tests/Localization/ShTest.php
+++ tests/Localization/ShTest.php
@@ -26,43 +26,43 @@ class ShTest extends AbstractTestCase
             $scope->assertSame('pre 1 sekund', $d->diffForHumans());
 
             $d = Carbon::now()->subSeconds(2);
-            $scope->assertSame('pre 2 sekund', $d->diffForHumans());
+            $scope->assertSame('pre 2 sekunda', $d->diffForHumans());
 
             $d = Carbon::now()->subMinutes(1);
             $scope->assertSame('pre 1 minut', $d->diffForHumans());
 
             $d = Carbon::now()->subMinutes(2);
-            $scope->assertSame('pre 2 minut', $d->diffForHumans());
+            $scope->assertSame('pre 2 minuta', $d->diffForHumans());
 
             $d = Carbon::now()->subHours(1);
             $scope->assertSame('pre 1 čas', $d->diffForHumans());
 
             $d = Carbon::now()->subHours(2);
-            $scope->assertSame('pre 2 čas', $d->diffForHumans());
+            $scope->assertSame('pre 2 časa', $d->diffForHumans());
 
             $d = Carbon::now()->subDays(1);
             $scope->assertSame('pre 1 dan', $d->diffForHumans());
 
             $d = Carbon::now()->subDays(2);
-            $scope->assertSame('pre 2 dan', $d->diffForHumans());
+            $scope->assertSame('pre 2 dana', $d->diffForHumans());
 
             $d = Carbon::now()->subWeeks(1);
             $scope->assertSame('pre 1 nedelja', $d->diffForHumans());
 
             $d = Carbon::now()->subWeeks(2);
-            $scope->assertSame('pre 2 nedelja', $d->diffForHumans());
+            $scope->assertSame('pre 2 nedelje', $d->diffForHumans());
 
             $d = Carbon::now()->subMonths(1);
             $scope->assertSame('pre 1 mesec', $d->diffForHumans());
 
             $d = Carbon::now()->subMonths(2);
-            $scope->assertSame('pre 2 mesec', $d->diffForHumans());
+            $scope->assertSame('pre 2 meseca', $d->diffForHumans());
 
             $d = Carbon::now()->subYears(1);
             $scope->assertSame('pre 1 godina', $d->diffForHumans());
 
             $d = Carbon::now()->subYears(2);
-            $scope->assertSame('pre 2 godina', $d->diffForHumans());
+            $scope->assertSame('pre 2 godine', $d->diffForHumans());
 
             $d = Carbon::now()->addSecond();
             $scope->assertSame('za 1 sekund', $d->diffForHumans());
@@ -73,7 +73,7 @@ class ShTest extends AbstractTestCase
             $scope->assertSame('1 sekund raniјe', $d2->diffForHumans($d));
 
             $scope->assertSame('1 sekund', $d->diffForHumans($d2, true));
-            $scope->assertSame('2 sekund', $d2->diffForHumans($d->addSecond(), true));
+            $scope->assertSame('2 sekunda', $d2->diffForHumans($d->addSecond(), true));
         });
     }
 }
