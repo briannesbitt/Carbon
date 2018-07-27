9166ea8f550c858cf75bf15e25cce4c4860be89d
KyleKatarn <kylekatarnls@gmail.com>
2018-07-27 13:10:06 +0200

    Add turkish meridiem

--- src/Carbon/Lang/tr.php
+++ src/Carbon/Lang/tr.php
@@ -82,6 +82,13 @@ return [
                 return $number.($suffixes[$lastDigit] ?? $suffixes[$number % 100 - $lastDigit] ?? $suffixes[$number >= 100 ? 100 : -1] ?? '');
         }
     },
+    'meridiem' => function($hours, $minutes, $isLower) {
+        if ($hours < 12) {
+            return $isLower ? 'öö' : 'ÖÖ';
+        }
+
+        return $isLower ? 'ös' : 'ÖS';
+    },
     'months' => ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
     'months_short' => ['Oca', 'Şub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'Ağu', 'Eyl', 'Eki', 'Kas', 'Ara'],
     'weekdays' => ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
