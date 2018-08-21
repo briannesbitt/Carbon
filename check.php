f57e7e02594536960172c1faaac33a1e15a9801c
KyleKatarn <kylekatarnls@gmail.com>
2018-08-21 18:00:56 +0200

    Document some more functions

--- check.php
+++ check.php
@@ -27,7 +27,7 @@ foreach (methods() as list($carbonObject, $className, $method, $parameters)) {
     $upperUnit = '(Unit|Micro(second)?s?|Milli(second)?s?|Seconds?|Minutes?|Hours?|Days?|Weeks?|Months?|Quarters?|Years?|Decades?|Centur(y|ies)|Millenni(um|a))';
     $lowerUnit = '(micro(second)?s?|milli(second)?s?|seconds?|minutes?|hours?|days?|weeks?|months?|quarters?|years?|decades?|centur(y|ies)|millenni(um|a))';
     $exclusion = !!preg_match("/^(
-        clone |
+        clone|setISODate |
         (use|is)StrictMode(Enabled)? |
         __(sleep|wakeup|construct) |
         (floor|ceil|round|sub(tract)?(Real)?|add(Real)?|isCurrent|isLast|isNext|isSame)$upperUnit?((No|With(No)?|Without)Overflow)? |
