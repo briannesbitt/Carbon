a56b6110d84317190e102bad1889c4d1ddffa16d
Robin D'Arcy <robin@klever.co.uk>
2018-11-23 17:42:51 +0000

    Fix style

--- src/Carbon/Traits/Comparison.php
+++ src/Carbon/Traits/Comparison.php
@@ -579,7 +579,7 @@ trait Comparison
                 // Backslash – the next character does not represent a date token so add it on as-is and continue.
                 // We're doing an extra ++$i here to increment the loop by 2.
                 if ($quotedFormat[$i] === '\\') {
-                    $regex .= '\\' . $quotedFormat[++$i];
+                    $regex .= '\\'.$quotedFormat[++$i];
                     continue;
                 }
 
