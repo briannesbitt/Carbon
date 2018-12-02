099ca37406098d729e5238ae4df1df94654494fc
KyleK <kylekatarnls@gmail.com>
2018-12-02 13:39:06 +0100

    Update references

--- tools/api-history.php
+++ tools/api-history.php
@@ -9,7 +9,7 @@ use Carbon\CarbonTimeZone;
 use Carbon\Laravel\ServiceProvider;
 
 const MASTER_BRANCH = 'dev-master';
-const MASTER_VERSION = '2.6.0';
+const MASTER_VERSION = '2.7.0';
 const BLACKLIST = [
     '1.23.1',
     '1.23.2',
