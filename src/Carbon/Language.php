1ee73d5f47056496f1ff504b47b16dd2d87a498f
KyleK <kylekatarnls@gmail.com>
2019-01-10 22:01:47 +0100

    Add Language class test

--- src/Carbon/Language.php
+++ src/Carbon/Language.php
@@ -136,6 +136,22 @@ class Language implements JsonSerializable
     /**
      * @return string|null
      */
+    public function getVariantName(): ?string
+    {
+        if ($this->variant === 'Latn') {
+            return 'Latin';
+        }
+
+        if ($this->variant === 'Cyrl') {
+            return 'Cyrillic';
+        }
+
+        return $this->variant;
+    }
+
+    /**
+     * @return string|null
+     */
     public function getRegion(): ?string
     {
         return $this->region;
@@ -162,6 +178,16 @@ class Language implements JsonSerializable
     }
 
     /**
+     * @param string $isoName
+     */
+    public function setIsoName(string $isoName): self
+    {
+        $this->isoName = $isoName;
+
+        return $this;
+    }
+
+    /**
      * @return string
      */
     public function getFullNativeName(): string
@@ -171,6 +197,16 @@ class Language implements JsonSerializable
         }
 
         return $this->nativeName;
+    }
+
+    /**
+     * @param string $nativeName
+     */
+    public function setNativeName(string $nativeName): self
+    {
+        $this->nativeName = $nativeName;
+
+        return $this;
     }
 
     /**
@@ -196,7 +232,7 @@ class Language implements JsonSerializable
     public function getIsoDescription()
     {
         $region = $this->getRegionName();
-        $variant = $this->getVariant();
+        $variant = $this->getVariantName();
 
         return $this->getIsoName().($region ? ' ('.$region.')' : '').($variant ? ' ('.$variant.')' : '');
     }
@@ -204,7 +240,7 @@ class Language implements JsonSerializable
     public function getNativeDescription()
     {
         $region = $this->getRegionName();
-        $variant = $this->getVariant();
+        $variant = $this->getVariantName();
 
         return $this->getNativeName().($region ? ' ('.$region.')' : '').($variant ? ' ('.$variant.')' : '');
     }
@@ -212,7 +248,7 @@ class Language implements JsonSerializable
     public function getFullIsoDescription()
     {
         $region = $this->getRegionName();
-        $variant = $this->getVariant();
+        $variant = $this->getVariantName();
 
         return $this->getFullIsoName().($region ? ' ('.$region.')' : '').($variant ? ' ('.$variant.')' : '');
     }
@@ -220,7 +256,7 @@ class Language implements JsonSerializable
     public function getFullNativeDescription()
     {
         $region = $this->getRegionName();
-        $variant = $this->getVariant();
+        $variant = $this->getVariantName();
 
         return $this->getFullNativeName().($region ? ' ('.$region.')' : '').($variant ? ' ('.$variant.')' : '');
     }
--- tests/Language/LanguageTest.php
+++ tests/Language/LanguageTest.php
@@ -18,9 +18,9 @@ class LanguageTest extends AbstractTestCase
     public function testAll()
     {
         $all = Language::all();
-        $this->assertIsArray($all);
+        $this->assertTrue(is_array($all));
         $this->assertArrayHasKey('en', $all);
-        $this->assertIsArray($all['en']);
+        $this->assertTrue(is_array($all['en']));
         $this->assertArrayHasKey('isoName', $all['en']);
         $this->assertSame('English', $all['en']['isoName']);
     }
@@ -28,7 +28,7 @@ class LanguageTest extends AbstractTestCase
     public function testRegions()
     {
         $regions = Language::regions();
-        $this->assertIsArray($regions);
+        $this->assertTrue(is_array($regions));
         $this->assertArrayHasKey('US', $regions);
         $this->assertSame('United States of America', $regions['US']);
     }
@@ -86,5 +86,164 @@ class LanguageTest extends AbstractTestCase
         $this->assertNull($ar->getRegion());
         $sr = new Language('sr_Cyrl_ME');
         $this->assertSame('ME', $sr->getRegion());
+    }
+
+    public function testGetRegionName()
+    {
+        $ar = new Language('ar');
+        $this->assertNull($ar->getRegionName());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('Algeria', $ar->getRegionName());
+        $ar = new Language('ar_Shakl');
+        $this->assertNull($ar->getRegionName());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('Montenegro', $sr->getRegionName());
+    }
+
+    public function testGetFullIsoName()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('Catalan, Valencian', $ar->getFullIsoName());
+        $this->assertSame('Catalan, Valencian', $ar->getFullIsoName());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('Arabic', $ar->getFullIsoName());
+        $gom = new Language('gom_Latn');
+        $this->assertSame('gom', $gom->getFullIsoName());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('Serbian', $sr->getFullIsoName());
+    }
+
+    public function testGetFullNativeName()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('català, valencià', $ar->getFullNativeName());
+        $this->assertSame('català, valencià', $ar->getFullNativeName());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('العربية', $ar->getFullNativeName());
+        $gom = new Language('gom_Latn');
+        $this->assertSame('gom', $gom->getFullNativeName());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('српски језик', $sr->getFullNativeName());
+    }
+
+    public function testGetIsoName()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('Catalan', $ar->getIsoName());
+        $this->assertSame('Catalan', $ar->getIsoName());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('Arabic', $ar->getIsoName());
+        $gom = new Language('gom_Latn');
+        $this->assertSame('gom', $gom->getIsoName());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('Serbian', $sr->getIsoName());
+    }
+
+    public function testGetNativeName()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('català', $ar->getNativeName());
+        $this->assertSame('català', $ar->getNativeName());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('العربية', $ar->getNativeName());
+        $gom = new Language('gom_Latn');
+        $this->assertSame('gom', $gom->getNativeName());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('српски језик', $sr->getNativeName());
+    }
+
+    public function testGetIsoDescription()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('Catalan', $ar->getIsoDescription());
+        $this->assertSame('Catalan', $ar->getIsoDescription());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('Arabic (Algeria)', $ar->getIsoDescription());
+        $gom = new Language('gom_Latn');
+        $this->assertSame('gom (Latin)', $gom->getIsoDescription());
+        $gom->setNativeName('Konkani');
+        $this->assertSame('gom (Latin)', $gom->getIsoDescription());
+        $gom->setIsoName('Konkani, Goan');
+        $this->assertSame('Konkani (Latin)', $gom->getIsoDescription());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('Serbian (Montenegro) (Cyrillic)', $sr->getIsoDescription());
+    }
+
+    public function testGetNativeDescription()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('català', $ar->getNativeDescription());
+        $this->assertSame('català', $ar->getNativeDescription());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('العربية (Algeria)', $ar->getNativeDescription());
+        $gom = new Language('gom_Latn');
+        $this->assertSame('gom (Latin)', $gom->getNativeDescription());
+        $gom->setIsoName('Konkani, Goan');
+        $this->assertSame('gom (Latin)', $gom->getNativeDescription());
+        $gom->setNativeName('Konkani');
+        $this->assertSame('Konkani (Latin)', $gom->getNativeDescription());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('српски језик (Montenegro) (Cyrillic)', $sr->getNativeDescription());
+    }
+
+    public function testGetFullIsoDescription()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('Catalan, Valencian', $ar->getFullIsoDescription());
+        $this->assertSame('Catalan, Valencian', $ar->getFullIsoDescription());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('Arabic (Algeria)', $ar->getFullIsoDescription());
+        $gom = new Language('gom_Latn');
+        $this->assertSame('gom (Latin)', $gom->getFullIsoDescription());
+        $gom->setNativeName('Konkani');
+        $this->assertSame('gom (Latin)', $gom->getFullIsoDescription());
+        $gom->setIsoName('Konkani, Goan');
+        $this->assertSame('Konkani, Goan (Latin)', $gom->getFullIsoDescription());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('Serbian (Montenegro) (Cyrillic)', $sr->getFullIsoDescription());
+    }
+
+    public function testGetFullNativeDescription()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('català, valencià', $ar->getFullNativeDescription());
+        $this->assertSame('català, valencià', $ar->getFullNativeDescription());
+        $ar = new Language('ar_DZ');
+        $this->assertSame('العربية (Algeria)', $ar->getFullNativeDescription());
+        $gom = new Language('gom_Latn');
+        $this->assertSame('gom (Latin)', $gom->getFullNativeDescription());
+        $gom->setIsoName('Konkani, Goan');
+        $this->assertSame('gom (Latin)', $gom->getFullNativeDescription());
+        $gom->setNativeName('Konkani, Goan');
+        $this->assertSame('Konkani, Goan (Latin)', $gom->getFullNativeDescription());
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('српски језик (Montenegro) (Cyrillic)', $sr->getFullNativeDescription());
+    }
+
+    public function testToString()
+    {
+        $ar = new Language('ar');
+        $this->assertSame('ar', strval($ar));
+        $ar = new Language('ar_DZ');
+        $this->assertSame('ar_DZ', strval($ar));
+        $ar = new Language('ar_Shakl');
+        $this->assertSame('ar_Shakl', strval($ar));
+    }
+
+    public function testToJson()
+    {
+        $ar = new Language('ca');
+        $this->assertSame('"Catalan"', json_encode($ar));
+        $this->assertSame('"Catalan"', json_encode($ar));
+        $ar = new Language('ar_DZ');
+        $this->assertSame('"Arabic (Algeria)"', json_encode($ar));
+        $gom = new Language('gom_Latn');
+        $this->assertSame('"gom (Latin)"', json_encode($gom));
+        $gom->setNativeName('Konkani');
+        $this->assertSame('"gom (Latin)"', json_encode($gom));
+        $gom->setIsoName('Konkani, Goan');
+        $this->assertSame('"Konkani (Latin)"', json_encode($gom));
+        $sr = new Language('sr_Cyrl_ME');
+        $this->assertSame('"Serbian (Montenegro) (Cyrillic)"', json_encode($sr));
     }
 }
