f3fb397f1b83aa1286de0e93aa27282b4ee061b5
KyleKatarn <kylekatarnls@gmail.com>
2019-02-18 10:41:31 +0100

    Restore nl period test

--- tests/Localization/HeTest.php
+++ tests/Localization/HeTest.php
@@ -12,7 +12,7 @@ namespace Tests\Localization;
 
 class HeTest extends LocalizationTestCase
 {
-    const LOCALE = 'he'; // Hebrew (modern]
+    const LOCALE = 'he'; // Hebrew (modern)
 
     const CASES = [
         // Carbon::parse('2018-01-04 00:00:00')->addDays(1)->calendar(Carbon::parse('2018-01-04 00:00:00'))
