3afdf80e65288e1205a7328a80740794f59c8fcd
KyleKatarn <kylekatarnls@gmail.com>
2019-02-26 10:48:58 +0100

    Fix #1616 Support aUnit option for Ukrainian language

--- src/Carbon/Lang/uk.php
+++ src/Carbon/Lang/uk.php
@@ -45,22 +45,30 @@ $processHoursFunction = function (\Carbon\CarbonInterface $date, string $format)
  * - Iwakura Taro
  * - Andrii Ponomarov
  * - alecrabbit
+ * - vystepanenko
  */
 return [
     'year' => ':count рік|:count роки|:count років',
     'y' => ':count рік|:count роки|:count років',
+    'a_year' => '{1}рік|:count рік|:count роки|:count років',
     'month' => ':count місяць|:count місяці|:count місяців',
     'm' => ':count місяць|:count місяці|:count місяців',
+    'a_month' => '{1}місяць|:count місяць|:count місяці|:count місяців',
     'week' => ':count тиждень|:count тижні|:count тижнів',
     'w' => ':count тиждень|:count тижні|:count тижнів',
+    'a_week' => '{1}тиждень|:count тиждень|:count тижні|:count тижнів',
     'day' => ':count день|:count дні|:count днів',
     'd' => ':count день|:count дні|:count днів',
+    'a_day' => '{1}день|:count день|:count дні|:count днів',
     'hour' => ':count година|:count години|:count годин',
     'h' => ':count година|:count години|:count годин',
+    'a_hour' => '{1}годину|:count година|:count години|:count годин',
     'minute' => ':count хвилину|:count хвилини|:count хвилин',
     'min' => ':count хвилину|:count хвилини|:count хвилин',
+    'a_minute' => '{1}хвилина|:count хвилину|:count хвилини|:count хвилин',
     'second' => ':count секунду|:count секунди|:count секунд',
     's' => ':count секунду|:count секунди|:count секунд',
+    'a_second' => '{0,1}кілька секунд|:count секунду|:count секунди|:count секунд',
     'ago' => ':time тому',
     'from_now' => 'за :time',
     'after' => ':time після',
