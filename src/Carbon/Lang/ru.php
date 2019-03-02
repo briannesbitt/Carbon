e415c72c847b809715f86604f87eba4733ed14db
KyleK <kylekatarnls@gmail.com>
2019-03-02 19:51:45 +0100

    Allow Laravel failure from TravisCI

--- src/Carbon/Lang/ru.php
+++ src/Carbon/Lang/ru.php
@@ -8,7 +8,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 $transformDiff = function ($input) {
     return strtr($input, [
         'неделя' => 'неделю',
@@ -16,7 +15,7 @@ $transformDiff = function ($input) {
     ]);
 };
 
-/**
+/*
  * Authors:
  * - Bari Badamshin
  * - Jørn Ølmheim
