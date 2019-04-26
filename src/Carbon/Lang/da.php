408bdf1db6af36c673e7c8d8e88bca38475bfdea
KyleK <kylekatarnls@gmail.com>
2019-04-27 00:59:13 +0200

    Fix #1703 Danish short month and short day

--- src/Carbon/Lang/da.php
+++ src/Carbon/Lang/da.php
@@ -17,6 +17,7 @@
  * - JD Isaacks
  * - Jens Herlevsen
  * - Ulrik McArdle (mcardle)
+ * - Frederik Sauer (FrittenKeeZ)
  */
 return [
     'year' => ':count år|:count år',
@@ -24,13 +25,13 @@ return [
     'y' => ':count år|:count år',
     'month' => ':count måned|:count måneder',
     'a_month' => 'en måned|:count måneder',
-    'm' => ':count mån.',
+    'm' => ':count mdr.',
     'week' => ':count uge|:count uger',
     'a_week' => 'en uge|:count uger',
     'w' => ':count u.',
     'day' => ':count dag|:count dage',
     'a_day' => ':count dag|:count dage',
-    'd' => 'en d.',
+    'd' => ':count d.',
     'hour' => ':count time|:count timer',
     'a_hour' => 'en time|:count timer',
     'h' => ':count t.',
