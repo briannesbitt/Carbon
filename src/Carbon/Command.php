d793ed00f545b364af7e83118cf58aaae0d072cf
KyleK <kylekatarnls@gmail.com>
2019-06-05 08:48:47 +0200

    Add CLI IO read/write utils

--- src/Carbon/Command.php
+++ src/Carbon/Command.php
@@ -2,7 +2,6 @@
 
 namespace Carbon;
 
-
 class Command
 {
     protected $colors = [
@@ -19,20 +18,77 @@ class Command
         'yellow' => '1;33',
     ];
 
+    protected $lastText = '';
+
+    protected $currentCompletion = null;
+
     public function __construct(array $colors = null)
     {
         if ($colors) {
             $this->colors = $colors;
         }
 
-        foreach ($this->colors as $name => $color) {
-            echo $this->colorize($name, $color)."\n";
+        if (function_exists('readline_completion_function')) {
+            readline_completion_function(function ($start) {
+                if (is_array($this->currentCompletion)) {
+                    $length = strlen($start);
+
+                    return array_filter($this->currentCompletion, function ($suggestion) use ($length, $start) {
+                        return substr($suggestion, 0, $length) === $start;
+                    });
+                }
+
+                return $this->currentCompletion ? $this->currentCompletion($start) : [];
+            });
         }
     }
 
-    protected function colorize($text, $color)
+    protected function colorize(string $text = '', string $color = null)
     {
         return "\033[${color}m$text\033[0m";
+    }
+
+    public function read($prompt, $completion = null)
+    {
+        $this->currentCompletion = $completion;
+
+        return readline($prompt);
+    }
+
+    public function rewind(int $length = null): void
+    {
+        if ($length === null) {
+            $length = strlen($this->lastText);
+        }
+
+        echo "\033[${length}D";
+    }
+
+    public function write(string $text = '', string $color = null): void
+    {
+        $this->lastText = $text;
+
+        if ($color) {
+            $text = $this->colorize($text, $color);
+        }
+
+        echo $text;
+    }
+
+    public function writeLine(string $text = '', string $color = null): void
+    {
+        $this->write("$text\n", $color);
+    }
+
+    public function rewrite(string $text = '', string $color = null): void
+    {
+        $this->rewind();
+        $this->write($text, $color);
+    }
+
+    public function rewriteLine(string $text = '', string $color = null): void
+    {
+        $this->write("\r$text", $color);
     }
 
     public function __invoke(...$parameters)
