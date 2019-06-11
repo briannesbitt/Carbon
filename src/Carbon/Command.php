abcf96e33c3d55e0d9e66291ddf3db45e37350d5
KyleKatarn <kylekatarnls@gmail.com>
2019-06-11 17:58:03 +0200

    Allow both codes and names for colors

--- src/Carbon/Command.php
+++ src/Carbon/Command.php
@@ -5,6 +5,8 @@ namespace Carbon;
 class Command
 {
     protected $colors = [
+        'black' => '0;30',
+        'dark_gray' => '1;30',
         'blue' => '0;34',
         'light_blue' => '1;34',
         'green' => '0;32',
@@ -15,17 +17,35 @@ class Command
         'light_red' => '1;31',
         'purple' => '0;35',
         'light_purple' => '1;35',
+        'brown' => '0;33',
         'yellow' => '1;33',
+        'light_gray' => '0;37',
+        'white' => '1;37',
+    ];
+
+    protected $backgrounds = [
+        'black' => '40',
+        'red' => '41',
+        'green' => '42',
+        'yellow' => '43',
+        'blue' => '44',
+        'magenta' => '45',
+        'cyan' => '46',
+        'light_gray' => '47',
     ];
 
     protected $lastText = '';
 
     protected $currentCompletion = null;
 
-    public function __construct(array $colors = null)
+    public function __construct(array $colors = null, array $backgrounds = null)
     {
         if ($colors) {
             $this->colors = $colors;
+        }
+
+        if ($backgrounds) {
+            $this->backgrounds = $backgrounds;
         }
 
         if (extension_loaded('readline') && function_exists('readline_completion_function')) {
@@ -46,9 +66,24 @@ class Command
         return $this->currentCompletion ? $this->currentCompletion($start) : [];
     }
 
-    protected function colorize(string $text = '', string $color = null)
+    protected function getColorCode(string $color, array $colors = null)
     {
-        return "\033[${color}m$text\033[0m";
+        $colors = $colors ?: $this->colors;
+        $color = $colors[$color] ?? $color;
+
+        return "\033[${color}m";
+    }
+
+    protected function colorize(string $text = '', string $color = null, string $background = null)
+    {
+        if (!$color && !$background) {
+            return $text;
+        }
+
+        $color = $color ? $this->getColorCode($color) : '';
+        $background = $background ? $this->getColorCode($background, $this->backgrounds) : '';
+
+        return "$color$background$text\033[0m";
     }
 
     public function read($prompt, $completion = null)
