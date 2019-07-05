4ae2a7d18a28f09777d70fbf13d9a92ebc012c9e
KyleKatarn <kylekatarnls@gmail.com>
2019-07-05 18:31:54 +0200

    Document parseFromLocale in reference coming in 2.21

--- tools/config.php
+++ tools/config.php
@@ -1,9 +1,10 @@
 <?php
 
 const MASTER_BRANCH = 'dev-master';
-const MASTER_VERSION = '2.16.3';
+const MASTER_VERSION = '2.21.0';
 const BLACKLIST = [
     '1.23.1',
     '1.23.2',
     '1.37.0',
+    '1.38.3',
 ];
