e53adff2b416d2b69c6a202ab950627e418c9fce
burobo <boronobu@gmail.com>
2019-07-13 23:22:02 +0900

    Add test cases for equalsTo method.

--- tests/CarbonInterval/ComparisonTest.php
+++ tests/CarbonInterval/ComparisonTest.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare (strict_types = 1);
 
 /**
  * This file is part of the Carbon package.
@@ -12,17 +12,27 @@ declare(strict_types = 1);
 namespace Tests\CarbonInterval;
 
 use Carbon\CarbonInterval;
+use DateInterval;
 use Tests\AbstractTestCase;
 
 class ComparisonTest extends AbstractTestCase
 {
     public function testEqualToTrue()
     {
-        $this->assertTrue(CarbonInterval::day()->eq(CarbonInterval::hours(24)));
+        $oneDay = CarbonInterval::day();
+        $this->assertTrue($oneDay->eq($oneDay));
+        $this->assertTrue($oneDay->eq(CarbonInterval::day()));
+        $this->assertTrue($oneDay->eq(new DateInterval('P1D')));
+        $this->assertTrue($oneDay->eq(CarbonInterval::hours(24)));
+        $this->assertTrue($oneDay->eq(CarbonInterval::hours(23)->minutes(60)));
+        $this->assertTrue($oneDay->eq('24 hours'));
+        $this->assertTrue($oneDay->eq('P1D'));
     }
 
     public function testEqualToFalse()
     {
-        $this->assertFalse(CarbonInterval::day()->eq(CarbonInterval::hours(23)));
+        $oneDay = CarbonInterval::day();
+        $this->assertFalse($oneDay->eq(CarbonInterval::hours(24)->microsecond(1)));
+        $this->assertFalse($oneDay->eq(CarbonInterval::hours(23)->minutes(59)->seconds(59)->microseconds(999999)));
     }
 }
