4ae40e21e3a2ae2eef69af9a468a9e55c3dc1105
KyleKatarn <kylekatarnls@gmail.com>
2019-07-15 10:40:26 +0200

    Add translation authors

--- src/Carbon/Lang/mr.php
+++ src/Carbon/Lang/mr.php
@@ -8,6 +8,11 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+/*
+ * Authors:
+ * - Vikram-enyota
+ */
 return [
     'year' => ':count वर्ष',
     'y' => ':count वर्ष',
