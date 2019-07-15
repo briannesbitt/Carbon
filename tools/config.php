a8ab15710203199b38ed8fabec26a7ce32475de0
KyleK <kylekatarnls@gmail.com>
2019-07-15 23:11:50 +0200

    Document 2.21 and 2.22 methods in API reference

--- tools/config.php
+++ tools/config.php
@@ -1,10 +1,11 @@
 <?php
 
 const MASTER_BRANCH = 'dev-master';
-const MASTER_VERSION = '2.21.0';
+const MASTER_VERSION = '2.22.0';
 const BLACKLIST = [
     '1.23.1',
     '1.23.2',
+    '1.25.2',
     '1.37.0',
     '1.38.3',
 ];
