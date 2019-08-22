93a7e62a812080f3f2a44f2a5c06116ae0737375
KyleK <kylekatarnls@gmail.com>
2019-08-22 22:29:33 +0200

    Fix #1861 Add aUnit option in Thai

--- tests/Localization/ThTest.php
+++ tests/Localization/ThTest.php
@@ -17,210 +17,415 @@ class ThTest extends LocalizationTestCase
 
     const CASES = [
         // Carbon::parse('2018-01-04 00:00:00')->addDays(1)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Tomorrow at 12:00 AM',
         'พรุ่งนี้ เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->addDays(2)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Saturday at 12:00 AM',
         'เสาร์หน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->addDays(3)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Sunday at 12:00 AM',
         'อาทิตย์หน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->addDays(4)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Monday at 12:00 AM',
         'จันทร์หน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->addDays(5)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Tuesday at 12:00 AM',
         'อังคารหน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->addDays(6)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Wednesday at 12:00 AM',
         'พุธหน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-05 00:00:00')->addDays(6)->calendar(Carbon::parse('2018-01-05 00:00:00'))
+        // 'Thursday at 12:00 AM',
         'พฤหัสบดีหน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-06 00:00:00')->addDays(6)->calendar(Carbon::parse('2018-01-06 00:00:00'))
+        // 'Friday at 12:00 AM',
         'ศุกร์หน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-07 00:00:00')->addDays(2)->calendar(Carbon::parse('2018-01-07 00:00:00'))
+        // 'Tuesday at 12:00 AM',
         'อังคารหน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-07 00:00:00')->addDays(3)->calendar(Carbon::parse('2018-01-07 00:00:00'))
+        // 'Wednesday at 12:00 AM',
         'พุธหน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-07 00:00:00')->addDays(4)->calendar(Carbon::parse('2018-01-07 00:00:00'))
+        // 'Thursday at 12:00 AM',
         'พฤหัสบดีหน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-07 00:00:00')->addDays(5)->calendar(Carbon::parse('2018-01-07 00:00:00'))
+        // 'Friday at 12:00 AM',
         'ศุกร์หน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-07 00:00:00')->addDays(6)->calendar(Carbon::parse('2018-01-07 00:00:00'))
+        // 'Saturday at 12:00 AM',
         'เสาร์หน้า เวลา 0:00',
+
         // Carbon::now()->subDays(2)->calendar()
+        // 'Last Sunday at 8:49 PM',
         'วันอาทิตย์ที่แล้ว เวลา 20:49',
+
         // Carbon::parse('2018-01-04 00:00:00')->subHours(2)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Yesterday at 10:00 PM',
         'เมื่อวานนี้ เวลา 22:00',
+
         // Carbon::parse('2018-01-04 12:00:00')->subHours(2)->calendar(Carbon::parse('2018-01-04 12:00:00'))
+        // 'Today at 10:00 AM',
         'วันนี้ เวลา 10:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->addHours(2)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Today at 2:00 AM',
         'วันนี้ เวลา 2:00',
+
         // Carbon::parse('2018-01-04 23:00:00')->addHours(2)->calendar(Carbon::parse('2018-01-04 23:00:00'))
+        // 'Tomorrow at 1:00 AM',
         'พรุ่งนี้ เวลา 1:00',
+
         // Carbon::parse('2018-01-07 00:00:00')->addDays(2)->calendar(Carbon::parse('2018-01-07 00:00:00'))
+        // 'Tuesday at 12:00 AM',
         'อังคารหน้า เวลา 0:00',
+
         // Carbon::parse('2018-01-08 00:00:00')->subDay()->calendar(Carbon::parse('2018-01-08 00:00:00'))
+        // 'Yesterday at 12:00 AM',
         'เมื่อวานนี้ เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->subDays(1)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Yesterday at 12:00 AM',
         'เมื่อวานนี้ เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->subDays(2)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Last Tuesday at 12:00 AM',
         'วันอังคารที่แล้ว เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->subDays(3)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Last Monday at 12:00 AM',
         'วันจันทร์ที่แล้ว เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->subDays(4)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Last Sunday at 12:00 AM',
         'วันอาทิตย์ที่แล้ว เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->subDays(5)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Last Saturday at 12:00 AM',
         'วันเสาร์ที่แล้ว เวลา 0:00',
+
         // Carbon::parse('2018-01-04 00:00:00')->subDays(6)->calendar(Carbon::parse('2018-01-04 00:00:00'))
+        // 'Last Friday at 12:00 AM',
         'วันศุกร์ที่แล้ว เวลา 0:00',
+
         // Carbon::parse('2018-01-03 00:00:00')->subDays(6)->calendar(Carbon::parse('2018-01-03 00:00:00'))
+        // 'Last Thursday at 12:00 AM',
         'วันพฤหัสบดีที่แล้ว เวลา 0:00',
+
         // Carbon::parse('2018-01-02 00:00:00')->subDays(6)->calendar(Carbon::parse('2018-01-02 00:00:00'))
+        // 'Last Wednesday at 12:00 AM',
         'วันพุธที่แล้ว เวลา 0:00',
+
         // Carbon::parse('2018-01-07 00:00:00')->subDays(2)->calendar(Carbon::parse('2018-01-07 00:00:00'))
+        // 'Last Friday at 12:00 AM',
         'วันศุกร์ที่แล้ว เวลา 0:00',
+
         // Carbon::parse('2018-01-01 00:00:00')->isoFormat('Qo Mo Do Wo wo')
+        // '1st 1st 1st 1st 1st',
         '1 1 1 1 1',
+
         // Carbon::parse('2018-01-02 00:00:00')->isoFormat('Do wo')
+        // '2nd 1st',
         '2 1',
+
         // Carbon::parse('2018-01-03 00:00:00')->isoFormat('Do wo')
+        // '3rd 1st',
         '3 1',
+
         // Carbon::parse('2018-01-04 00:00:00')->isoFormat('Do wo')
+        // '4th 1st',
         '4 1',
+
         // Carbon::parse('2018-01-05 00:00:00')->isoFormat('Do wo')
+        // '5th 1st',
         '5 1',
+
         // Carbon::parse('2018-01-06 00:00:00')->isoFormat('Do wo')
+        // '6th 1st',
         '6 1',
+
         // Carbon::parse('2018-01-07 00:00:00')->isoFormat('Do wo')
+        // '7th 2nd',
         '7 2',
+
         // Carbon::parse('2018-01-11 00:00:00')->isoFormat('Do wo')
+        // '11th 2nd',
         '11 2',
+
         // Carbon::parse('2018-02-09 00:00:00')->isoFormat('DDDo')
+        // '40th',
         '40',
+
         // Carbon::parse('2018-02-10 00:00:00')->isoFormat('DDDo')
+        // '41st',
         '41',
+
         // Carbon::parse('2018-04-10 00:00:00')->isoFormat('DDDo')
+        // '100th',
         '100',
+
         // Carbon::parse('2018-02-10 00:00:00', 'Europe/Paris')->isoFormat('h:mm a z')
+        // '12:00 am cet',
         '12:00 ก่อนเที่ยง cet',
+
         // Carbon::parse('2018-02-10 00:00:00')->isoFormat('h:mm A, h:mm a')
+        // '12:00 AM, 12:00 am',
         '12:00 ก่อนเที่ยง, 12:00 ก่อนเที่ยง',
+
         // Carbon::parse('2018-02-10 01:30:00')->isoFormat('h:mm A, h:mm a')
+        // '1:30 AM, 1:30 am',
         '1:30 ก่อนเที่ยง, 1:30 ก่อนเที่ยง',
+
         // Carbon::parse('2018-02-10 02:00:00')->isoFormat('h:mm A, h:mm a')
+        // '2:00 AM, 2:00 am',
         '2:00 ก่อนเที่ยง, 2:00 ก่อนเที่ยง',
+
         // Carbon::parse('2018-02-10 06:00:00')->isoFormat('h:mm A, h:mm a')
+        // '6:00 AM, 6:00 am',
         '6:00 ก่อนเที่ยง, 6:00 ก่อนเที่ยง',
+
         // Carbon::parse('2018-02-10 10:00:00')->isoFormat('h:mm A, h:mm a')
+        // '10:00 AM, 10:00 am',
         '10:00 ก่อนเที่ยง, 10:00 ก่อนเที่ยง',
+
         // Carbon::parse('2018-02-10 12:00:00')->isoFormat('h:mm A, h:mm a')
+        // '12:00 PM, 12:00 pm',
         '12:00 หลังเที่ยง, 12:00 หลังเที่ยง',
+
         // Carbon::parse('2018-02-10 17:00:00')->isoFormat('h:mm A, h:mm a')
+        // '5:00 PM, 5:00 pm',
         '5:00 หลังเที่ยง, 5:00 หลังเที่ยง',
+
         // Carbon::parse('2018-02-10 21:30:00')->isoFormat('h:mm A, h:mm a')
+        // '9:30 PM, 9:30 pm',
         '9:30 หลังเที่ยง, 9:30 หลังเที่ยง',
+
         // Carbon::parse('2018-02-10 23:00:00')->isoFormat('h:mm A, h:mm a')
+        // '11:00 PM, 11:00 pm',
         '11:00 หลังเที่ยง, 11:00 หลังเที่ยง',
+
         // Carbon::parse('2018-01-01 00:00:00')->ordinal('hour')
+        // '0th',
         '0',
+
         // Carbon::now()->subSeconds(1)->diffForHumans()
-        'ไม่กี่วินาทีที่แล้ว',
-        // Carbon::now()->subSeconds(1)->diffForHumans(null, false, true)
+        // '1 second ago',
         '1 วินาทีที่แล้ว',
+
+        // Carbon::now()->subSeconds(1)->diffForHumans(null, false, true)
+        // '1s ago',
+        '1 วินาทีที่แล้ว',
+
         // Carbon::now()->subSeconds(2)->diffForHumans()
+        // '2 seconds ago',
         '2 วินาทีที่แล้ว',
+
         // Carbon::now()->subSeconds(2)->diffForHumans(null, false, true)
+        // '2s ago',
         '2 วินาทีที่แล้ว',
+
         // Carbon::now()->subMinutes(1)->diffForHumans()
+        // '1 minute ago',
         '1 นาทีที่แล้ว',
+
         // Carbon::now()->subMinutes(1)->diffForHumans(null, false, true)
+        // '1m ago',
         '1 นาทีที่แล้ว',
+
         // Carbon::now()->subMinutes(2)->diffForHumans()
+        // '2 minutes ago',
         '2 นาทีที่แล้ว',
+
         // Carbon::now()->subMinutes(2)->diffForHumans(null, false, true)
+        // '2m ago',
         '2 นาทีที่แล้ว',
+
         // Carbon::now()->subHours(1)->diffForHumans()
+        // '1 hour ago',
         '1 ชั่วโมงที่แล้ว',
+
         // Carbon::now()->subHours(1)->diffForHumans(null, false, true)
+        // '1h ago',
         '1 ชั่วโมงที่แล้ว',
+
         // Carbon::now()->subHours(2)->diffForHumans()
+        // '2 hours ago',
         '2 ชั่วโมงที่แล้ว',
+
         // Carbon::now()->subHours(2)->diffForHumans(null, false, true)
+        // '2h ago',
         '2 ชั่วโมงที่แล้ว',
+
         // Carbon::now()->subDays(1)->diffForHumans()
+        // '1 day ago',
         '1 วันที่แล้ว',
+
         // Carbon::now()->subDays(1)->diffForHumans(null, false, true)
+        // '1d ago',
         '1 วันที่แล้ว',
+
         // Carbon::now()->subDays(2)->diffForHumans()
+        // '2 days ago',
         '2 วันที่แล้ว',
+
         // Carbon::now()->subDays(2)->diffForHumans(null, false, true)
+        // '2d ago',
         '2 วันที่แล้ว',
+
         // Carbon::now()->subWeeks(1)->diffForHumans()
+        // '1 week ago',
         '1 สัปดาห์ที่แล้ว',
+
         // Carbon::now()->subWeeks(1)->diffForHumans(null, false, true)
+        // '1w ago',
         '1 สัปดาห์ที่แล้ว',
+
         // Carbon::now()->subWeeks(2)->diffForHumans()
+        // '2 weeks ago',
         '2 สัปดาห์ที่แล้ว',
+
         // Carbon::now()->subWeeks(2)->diffForHumans(null, false, true)
+        // '2w ago',
         '2 สัปดาห์ที่แล้ว',
+
         // Carbon::now()->subMonths(1)->diffForHumans()
+        // '1 month ago',
         '1 เดือนที่แล้ว',
+
         // Carbon::now()->subMonths(1)->diffForHumans(null, false, true)
+        // '1mo ago',
         '1 เดือนที่แล้ว',
+
         // Carbon::now()->subMonths(2)->diffForHumans()
+        // '2 months ago',
         '2 เดือนที่แล้ว',
+
         // Carbon::now()->subMonths(2)->diffForHumans(null, false, true)
+        // '2mos ago',
         '2 เดือนที่แล้ว',
+
         // Carbon::now()->subYears(1)->diffForHumans()
+        // '1 year ago',
         '1 ปีที่แล้ว',
+
         // Carbon::now()->subYears(1)->diffForHumans(null, false, true)
+        // '1yr ago',
         '1 ปีที่แล้ว',
+
         // Carbon::now()->subYears(2)->diffForHumans()
+        // '2 years ago',
         '2 ปีที่แล้ว',
+
         // Carbon::now()->subYears(2)->diffForHumans(null, false, true)
+        // '2yrs ago',
         '2 ปีที่แล้ว',
+
         // Carbon::now()->addSecond()->diffForHumans()
-        'อีก ไม่กี่วินาที',
+        // '1 second from now',
+        'อีก 1 วินาที',
+
         // Carbon::now()->addSecond()->diffForHumans(null, false, true)
+        // '1s from now',
         'อีก 1 วินาที',
+
         // Carbon::now()->addSecond()->diffForHumans(Carbon::now())
-        'ไม่กี่วินาทีหลังจากนี้',
-        // Carbon::now()->addSecond()->diffForHumans(Carbon::now(), false, true)
+        // '1 second after',
         '1 วินาทีหลังจากนี้',
+
+        // Carbon::now()->addSecond()->diffForHumans(Carbon::now(), false, true)
+        // '1s after',
+        '1 วินาทีหลังจากนี้',
+
         // Carbon::now()->diffForHumans(Carbon::now()->addSecond())
-        'ไม่กี่วินาทีก่อน',
-        // Carbon::now()->diffForHumans(Carbon::now()->addSecond(), false, true)
+        // '1 second before',
         '1 วินาทีก่อน',
+
+        // Carbon::now()->diffForHumans(Carbon::now()->addSecond(), false, true)
+        // '1s before',
+        '1 วินาทีก่อน',
+
         // Carbon::now()->addSecond()->diffForHumans(Carbon::now(), true)
-        'ไม่กี่วินาที',
-        // Carbon::now()->addSecond()->diffForHumans(Carbon::now(), true, true)
+        // '1 second',
         '1 วินาที',
+
+        // Carbon::now()->addSecond()->diffForHumans(Carbon::now(), true, true)
+        // '1s',
+        '1 วินาที',
+
         // Carbon::now()->diffForHumans(Carbon::now()->addSecond()->addSecond(), true)
+        // '2 seconds',
         '2 วินาที',
+
         // Carbon::now()->diffForHumans(Carbon::now()->addSecond()->addSecond(), true, true)
+        // '2s',
         '2 วินาที',
+
         // Carbon::now()->addSecond()->diffForHumans(null, false, true, 1)
+        // '1s from now',
         'อีก 1 วินาที',
+
         // Carbon::now()->addMinute()->addSecond()->diffForHumans(null, true, false, 2)
-        '1 นาที ไม่กี่วินาที',
+        // '1 minute 1 second',
+        '1 นาที 1 วินาที',
+
         // Carbon::now()->addYears(2)->addMonths(3)->addDay()->addSecond()->diffForHumans(null, true, true, 4)
+        // '2yrs 3mos 1d 1s',
         '2 ปี 3 เดือน 1 วัน 1 วินาที',
+
         // Carbon::now()->addYears(3)->diffForHumans(null, null, false, 4)
+        // '3 years from now',
         'อีก 3 ปี',
+
         // Carbon::now()->subMonths(5)->diffForHumans(null, null, true, 4)
+        // '5mos ago',
         '5 เดือนที่แล้ว',
+
         // Carbon::now()->subYears(2)->subMonths(3)->subDay()->subSecond()->diffForHumans(null, null, true, 4)
+        // '2yrs 3mos 1d 1s ago',
         '2 ปี 3 เดือน 1 วัน 1 วินาทีที่แล้ว',
+
         // Carbon::now()->addWeek()->addHours(10)->diffForHumans(null, true, false, 2)
+        // '1 week 10 hours',
         '1 สัปดาห์ 10 ชั่วโมง',
+
         // Carbon::now()->addWeek()->addDays(6)->diffForHumans(null, true, false, 2)
+        // '1 week 6 days',
         '1 สัปดาห์ 6 วัน',
+
         // Carbon::now()->addWeek()->addDays(6)->diffForHumans(null, true, false, 2)
+        // '1 week 6 days',
         '1 สัปดาห์ 6 วัน',
+
         // Carbon::now()->addWeek()->addDays(6)->diffForHumans(["join" => true, "parts" => 2])
+        // '1 week and 6 days from now',
         'อีก 1 สัปดาห์ และ 6 วัน',
+
         // Carbon::now()->addWeeks(2)->addHour()->diffForHumans(null, true, false, 2)
+        // '2 weeks 1 hour',
         '2 สัปดาห์ 1 ชั่วโมง',
+
         // Carbon::now()->addHour()->diffForHumans(["aUnit" => true])
+        // 'an hour from now',
         'อีก 1 ชั่วโมง',
+
         // CarbonInterval::days(2)->forHumans()
+        // '2 days',
         '2 วัน',
+
         // CarbonInterval::create('P1DT3H')->forHumans(true)
+        // '1d 3h',
         '1 วัน 3 ชั่วโมง',
     ];
 }
