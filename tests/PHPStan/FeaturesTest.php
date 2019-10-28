ed8431096bd4c845f8fc7208b4c56efd5e5676c0
KyleKatarn <kylekatarnls@gmail.com>
2019-10-28 13:30:26 +0100

    Allow to run PHPStan tests on Windows

--- tests/PHPStan/FeaturesTest.php
+++ tests/PHPStan/FeaturesTest.php
@@ -30,15 +30,15 @@ class PHPStanTest extends AbstractTestCase
 
     private function analyze(string $file)
     {
-        $output =  shell_exec(
-            'vendor/bin/phpstan'
-            . ' analyse'
-            . ' --configuration="' . __DIR__ . '/../../extension.neon"'
-            . ' --no-progress'
-            . ' --no-interaction'
-            . ' --level=0'
-            . ' "' . $file . '"'
-        );
+        $output = shell_exec(implode(' ', [
+            implode(DIRECTORY_SEPARATOR, ['.', 'vendor', 'bin', 'phpstan']),
+            'analyse',
+            '--configuration='.escapeshellarg(realpath(__DIR__.'/../../extension.neon')),
+            '--no-progress',
+            '--no-interaction',
+            '--level=0',
+            escapeshellarg(realpath($file)),
+        ]));
         var_dump($output);
 
         return $output;
