95ef0aae5e810a191df19b1bc7e3efa0c4fbf3fc
KyleKatarn <kylekatarnls@gmail.com>
2019-10-28 13:29:48 +0100

    Allow to run the test from any working directory

--- tests/PHPStan/FeaturesTest.php
+++ tests/PHPStan/FeaturesTest.php
@@ -6,9 +6,24 @@ namespace Tests;
 
 class PHPStanTest extends AbstractTestCase
 {
+    protected $phpStanPreviousDirectory = '.';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->phpStanPreviousDirectory = getcwd();
+        chdir(__DIR__.'/../..');
+    }
+
+    protected function tearDown(): void
+    {
+        chdir($this->phpStanPreviousDirectory);
+        parent::tearDown();
+    }
+
     public function testAnalysesWithoutErrors(): void
     {
-        if ($this->analyze(__DIR__ . '/Fixture.php') === 0) {
+        if ($this->analyze(__DIR__.'/Fixture.php') === 0) {
             $this->assertTrue(true);
         }
     }
