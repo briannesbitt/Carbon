2885469ab3feb34819f88e3a5e0d7d20913087ef
KyleKatarn <kylekatarnls@gmail.com>
2020-01-21 13:42:43 +0100

    Document 2.29.0 and 2.29.1

--- tools/check.php
+++ tools/check.php
@@ -28,9 +28,9 @@ foreach (methods(true) as list($carbonObject, $className, $method, $parameters))
     $lowerUnit = '(micro(second)?s?|milli(second)?s?|seconds?|minutes?|hours?|days?|weeks?|months?|quarters?|years?|decades?|centur(y|ies)|millenni(um|a))';
     $exclusion = (bool) preg_match("/^(
         baseMixin|clone|setISODate |
-        getTranslationMessage|getTranslationMessageWith|translateWith|getCalendarFormats|getPaddedUnit|translate|getFormatsToIsoReplacements|getTimeFormatByPrecision |
+        getTranslationMessage|getTranslationMessageWith|translateWith|getCalendarFormats|getPaddedUnit|translate|getFormatsToIsoReplacements|getTimeFormatByPrecision|hasLocalTranslator |
         (use|is)StrictMode(Enabled)? |
-        __(sleep|wakeup|construct|debugInfo|set_state) |
+        __(clone|sleep|wakeup|construct|debugInfo|set_state) |
         (floor|ceil|round|sub(tract)?(Real)?|add(Real)?|isCurrent|isLast|isNext|isSame)$upperUnit?((No|With(No)?|Without)Overflow)? |
         (set|get)$upperUnit |
         (startOf|endOf)$upperUnit |
