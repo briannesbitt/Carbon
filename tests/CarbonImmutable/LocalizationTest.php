3ff4d4f0ef9fbc77d0969b85c00bcae65ed4a079
KyleKatarn <kylekatarnls@gmail.com>
2020-04-13 12:23:51 +0200

    Fix #2065 Symfony version of the skip message for locale aware exception

--- tests/CarbonImmutable/LocalizationTest.php
+++ tests/CarbonImmutable/LocalizationTest.php
@@ -16,7 +16,6 @@ use Carbon\CarbonInterval;
 use Carbon\Exceptions\NotLocaleAwareException;
 use Carbon\Language;
 use Carbon\Translator;
-use ReflectionMethod;
 use Symfony\Component\Translation\IdentityTranslator;
 use Symfony\Component\Translation\Loader\ArrayLoader;
 use Symfony\Component\Translation\MessageCatalogue;
@@ -665,25 +664,15 @@ class LocalizationTest extends AbstractTestCase
     public function testNotLocaleAwareException()
     {
         if (method_exists(TranslatorInterface::class, 'getLocale')) {
-            $this->markTestSkipped('In Symfony < 4, NotLocaleAwareException will never been thrown.');
+            $this->markTestSkipped('In Symfony < 5, NotLocaleAwareException will never been thrown.');
         }
 
-        $parameters = (new ReflectionMethod(TranslatorInterface::class, 'trans'))->getParameters();
-
-        /** @var TranslatorInterface $translator */
-        $translator = $parameters[0]->getType()->getName() === 'string'
-            ? (new class implements TranslatorInterface {
-                public function trans(string $id, array $parameters = [], string $domain = null, string $locale = null)
-                {
-                    return 'x';
-                }
-            })
-            : (new class implements TranslatorInterface {
-                public function trans($id, array $parameters = [], $domain = null, $locale = null)
-                {
-                    return 'x';
-                }
-            });
+        $translator = new class implements TranslatorInterface {
+            public function trans(string $id, array $parameters = [], string $domain = null, string $locale = null)
+            {
+                return 'x';
+            }
+        };
 
         Carbon::setTranslator($translator);
 
