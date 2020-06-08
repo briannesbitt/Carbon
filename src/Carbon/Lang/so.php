6ee19e3eb2020de737c77d0bdbcc114db2ee4bed
Abdifatah Abdilahi <faahid44@hotmail.com>
2020-06-08 12:53:00 +0300

    Added full somali locale
    I added full somali translation, and it fully works for diffFOrHumans time format.

--- src/Carbon/Lang/so.php
+++ src/Carbon/Lang/so.php
@@ -10,6 +10,50 @@
  */
 
 /*
- * Unknown default region, use the first alphabetically.
+ * Author:
+ * - Abdifatah Abdilahi(@abdifatahz)
  */
-return require __DIR__.'/so_DJ.php';
+return [
+    'year' => '{1}:count sanad|{0}:count sanadood|]1,Inf[:count sanadood',
+    'a_year' => '{1}a sanad|{0}:count sanadood|]1,Inf[:count sanadood',
+    'y' => '{1}:countsn|{0}:countsns|]1,Inf[:countsn',
+    'month' => '{1}:count bil|{0}:count bilood|]1,Inf[:count bilood',
+    'a_month' => '{1}a bil|{0}:count bilood|]1,Inf[:count bilood',
+    'm' => '{1}:countbil|{0}:countbil|]1,Inf[:countbil',
+    'week' => '{1}:count isbuuc|{0}:count isbuuc|]1,Inf[:count isbuuc',
+    'a_week' => '{1}a week|{0}:count isbuuc|]1,Inf[:count isbuuc',
+    'w' => ':countis',
+    'day' => '{1}:count maalin|{0}:count maalmood|]1,Inf[:count maalmood',
+    'a_day' => '{1}maalin|{0}:count maalmood|]1,Inf[:count maalmood',
+    'd' => ':countml',
+    'hour' => '{1}:count saac|{0}:count saac|]1,Inf[:count saac',
+    'a_hour' => '{1}saacad|{0}:count saac|]1,Inf[:count saac',
+    'h' => ':countsc',
+    'minute' => '{1}:count daqiiqo|{0}:count daqiiqo|]1,Inf[:count daqiiqo',
+    'a_minute' => '{1}a daqiiqo|{0}:count daqiiqo|]1,Inf[:count daqiiqo',
+    'min' => ':countdq',
+    'second' => '{1}:count ilbidhiqsi|{0}:count ilbidhiqsi|]1,Inf[:count ilbidhiqsi',
+    'a_second' => '{1}a xooga ilbidhiqsiyo|{0}:count ilbidhiqsi|]1,Inf[:count ilbidhiqsi',
+    's' => ':countil',
+    'ago' => ':time kahor',
+    'from_now' => ':time ayaa kasoo wareegtay',
+    'after' => ':time kedib',
+    'before' => ':time kahor',
+    'diff_now' => 'hada',
+    'diff_yesterday' => 'shalayto',
+    'diff_tomorrow' => 'beri',
+    'diff_before_yesterday' => 'doraato',
+    'diff_after_tomorrow' => 'saadanbe',
+    'period_recurrences' => '{1}once|{0}:count times|]1,Inf[:count times',
+    'period_interval' => 'every :interval',
+    'period_start_date' => 'from :date',
+    'period_end_date' => 'to :date',
+    'months' => ['Janaayo ', 'Febraayo ', 'Abriil ', 'Maajo', 'Juun', 'Luuliyo', 'Agoosto', 'Sebteembar', 'Oktoobar', 'Nofeembar', 'Diseembar'],
+    'months_short' => ['Jan', 'Feb', 'Mar', 'Abr', 'Mjo', 'Jun', 'Lyo', 'Agt', 'Seb', 'Okt', 'Nof', 'Dis'],
+    'weekdays' => ['Axad', 'Isniin', 'Talaada', 'Arbaca', 'Khamiis', 'Jimce', 'Sabti'],
+    'weekdays_short' => ['Axd', 'Isn', 'Tal', 'Arb', 'Kha', 'Jim', 'Sbt'],
+    'weekdays_min' => ['Ax', 'Is', 'Ta', 'Ar', 'Kh', 'Ji', 'Sa'],
+    'list' => [', ', ' and '],
+    'first_day_of_week' => 6,
+    'day_of_first_week_of_year' => 1,
+];
