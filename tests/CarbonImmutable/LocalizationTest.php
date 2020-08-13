ed6c51a19bddae6d661854a2d15e1e3e52fd0e82
KyleKatarn <kylekatarnls@gmail.com>
2020-08-13 22:42:16 +0200

    Update unit tests

--- tests/CarbonImmutable/LocalizationTest.php
+++ tests/CarbonImmutable/LocalizationTest.php
@@ -684,7 +684,7 @@ class LocalizationTest extends AbstractTestCase
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage(
-            'Translator does not implement Symfony\Component\Translation\TranslatorInterface '.
+            'Translator does not implement Symfony\Contracts\Translation\TranslatorInterface '.
             'and Symfony\Component\Translation\TranslatorBagInterface. '.
             'Symfony\Component\Translation\IdentityTranslator has been given.'
         );
