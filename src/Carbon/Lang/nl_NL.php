a5cc12808d05d6b855ae049b62c24c3364957a9b
KyleKatarn <kylekatarnls@gmail.com>
2020-11-05 23:25:23 +0100

    Fix #2232 Use 4-digits years in nl_NL (as for other nl regions)

--- src/Carbon/Lang/nl_NL.php
+++ src/Carbon/Lang/nl_NL.php
@@ -14,9 +14,6 @@
  * - RAP    bug-glibc-locales@gnu.org
  */
 return array_replace_recursive(require __DIR__.'/nl.php', [
-    'formats' => [
-        'L' => 'DD-MM-YY',
-    ],
     'months' => ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'],
     'months_short' => ['jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'],
     'weekdays' => ['zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag'],
