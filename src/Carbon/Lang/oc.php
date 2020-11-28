d0463779663437392fe42ff339ebc0213bd55498
Kyle <kylekatarnls@users.noreply.github.com>
2020-11-28 15:25:28 +0100

    Fix #2240 fix Occitan ordinal rule (#2241)
    Fix #2240

--- src/Carbon/Lang/oc.php
+++ src/Carbon/Lang/oc.php
@@ -83,17 +83,14 @@ return [
     'weekdays_short' => ['dg', 'dl', 'dm', 'dc', 'dj', 'dv', 'ds'],
     'weekdays_min' => ['dg', 'dl', 'dm', 'dc', 'dj', 'dv', 'ds'],
     'ordinal' => function ($number, $period) {
-        return $number.(
-            ($period === 'w' || $period === 'W') ? 'a' : (
-                ($number === 1) ? 'r' : (
-                    ($number === 2) ? 'n' : (
-                        ($number === 3) ? 'r' : (
-                            ($number === 4) ? 't' : 'è'
-                        )
-                    )
-                )
-            )
-        );
+        $ordinal = [1 => 'èr', 2 => 'nd'][(int) $number] ?? 'en';
+
+        // feminine for year, week, hour, minute, second
+        if (preg_match('/^[yYwWhHgGis]$/', $period)) {
+            $ordinal .= 'a';
+        }
+
+        return $number.$ordinal;
     },
     'first_day_of_week' => 1,
     'day_of_first_week_of_year' => 4,
