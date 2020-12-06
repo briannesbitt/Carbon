ed6e58fd8dec11c2e201e6bbe570aa12117f8232
KyleKatarn <kylekatarnls@gmail.com>
2020-12-06 13:24:54 +0100

    Add composer release command

---
+++ tools/release.php
@@ -0,0 +1,26 @@
+<?php
+
+$releases = json_decode(file_get_contents(__DIR__ . '/../releases.json'));
+
+$composer = file_get_contents(__DIR__ . '/../composer.json');
+
+$regExp = '/"nesbot\/carbon":\s*"[^"]+"/';
+
+if (!preg_match($regExp, $composer)) {
+    echo "nesbot/carbon not found in composer.json.\n";
+    exit(1);
+}
+
+$newComposer = preg_replace(
+    $regExp,
+    '"nesbot/carbon": "dev-master as ' . $releases[0]->tag_name . '"',
+    $composer
+);
+
+echo $newComposer === $composer
+    ? 'nesbot/carbon is already on '
+    : 'Upgraded nesbot/carbon to ';
+
+echo $releases[0]->tag_name . ".\n";
+
+exit(0);
