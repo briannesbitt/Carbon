df31a6512a94064bde12ad2551a42042f0cb5f38
KyleKatarn <kylekatarnls@gmail.com>
2021-05-22 23:26:47 +0200

    Restore localization group annotation

--- tests/Localization/BeByTest.php
+++ tests/Localization/BeByTest.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
  */
 namespace Tests\Localization;
 
+/**
+ * @group localization
+ */
 class BeByTest extends LocalizationTestCase
 {
     public const LOCALE = 'be_BY'; // Belarusian
--- tests/Localization/BeTest.php
+++ tests/Localization/BeTest.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
  */
 namespace Tests\Localization;
 
+/**
+ * @group localization
+ */
 class BeTest extends LocalizationTestCase
 {
     public const LOCALE = 'be'; // Belarusian
