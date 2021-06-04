8227163971c29bda19d6d81c6ef1b75236b30857
kylekatarnls <kylekatarnls@gmail.com>
2021-06-04 21:20:14 +0200

    Work around PHP bug 81106

--- src/Carbon/Traits/Units.php
+++ src/Carbon/Traits/Units.php
@@ -210,6 +210,14 @@ trait Units
         }
 
         if ($unit instanceof DateInterval) {
+            // Can be removed if https://bugs.php.net/bug.php?id=81106
+            // is fixed
+            if (version_compare(PHP_VERSION, '8.1.0-dev', '>=') && ($unit->f < 0 || $unit->f >= 1)) {
+                $seconds = floor($unit->f);
+                $unit->f -= $seconds;
+                $unit->s += $seconds;
+            }
+
             return parent::add($unit);
         }
 
