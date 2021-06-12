ff3123c88c6eb3d3c317d9296154d77822425ab0
KyleKatarn <kylekatarnls@gmail.com>
2021-06-12 13:03:21 +0200

    Sort __set_state test in its own file

---
+++ tests/CarbonImmutable/SetStateTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Carbon package.
+ *
+ * (c) Brian Nesbitt <brian@nesbot.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\CarbonImmutable;
+
+use Carbon\Traits\Serialization;
+use stdClass;
+use Tests\AbstractTestCase;
+
+class SetStateTest extends AbstractTestCase
+{
+    public function testSteState()
+    {
+        $obj = new class() {
+            use Serialization;
+
+            public static function instance($value)
+            {
+                return $value;
+            }
+
+            public function callSetState($value)
+            {
+                return static::__set_state($value);
+            }
+        };
+
+        $data = $obj->callSetState(['foo' => 'bar']);
+
+        $this->assertInstanceOf(stdClass::class, $data);
+        $this->assertSame(['foo' => 'bar'], (array) $data);
+    }
+}
--- tests/CarbonInterval/ToStringTest.php
+++ tests/CarbonInterval/ToStringTest.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 namespace Tests\CarbonInterval;
 
 use Carbon\CarbonInterval;
-use Carbon\Traits\Serialization;
-use stdClass;
 use Tests\AbstractTestCase;
 
 class ToStringTest extends AbstractTestCase
@@ -60,27 +58,5 @@ class ToStringTest extends AbstractTestCase
             return 'Y'.($interval->years * 2);
         }]);
         $this->assertSame('Y22:abc', $ci.':abc');
-    }
-
-    public function testSteState()
-    {
-        $obj = new class() {
-            use Serialization;
-
-            public static function instance($value)
-            {
-                return $value;
-            }
-
-            public function callSetState($value)
-            {
-                return static::__set_state($value);
-            }
-        };
-
-        $data = $obj->callSetState(['foo' => 'bar']);
-
-        $this->assertInstanceOf(stdClass::class, $data);
-        $this->assertSame(['foo' => 'bar'], (array) $data);
     }
 }
