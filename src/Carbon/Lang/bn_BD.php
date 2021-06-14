7f6c0d62a70ea8c8ee7187aa23a902709becf892
Lucas Michot <lucas@semalead.com>
2021-06-14 14:15:07 +0200

    Re-add group and author names

--- src/Carbon/Lang/bn_BD.php
+++ src/Carbon/Lang/bn_BD.php
@@ -9,6 +9,10 @@
  * file that was distributed with this source code.
  */
 
+/*
+ * Authors:
+ * - Ankur Group, Taneem Ahmed, Jamil Ahmed
+ */
 return array_replace_recursive(require __DIR__.'/bn.php', [
     'formats' => [
         'L' => 'D/M/YY',
