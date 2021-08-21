0dead96e34308858304d459892fbd0f04b888d49
Lucas Michot <lucas@semalead.com>
2021-08-21 14:40:38 +0200

    Add missing u regexp modifier

--- src/Carbon/Lang/nl.php
+++ src/Carbon/Lang/nl.php
@@ -71,9 +71,9 @@ return [
     'period_recurrences' => ':count keer',
     'period_interval' => function (string $interval = '') {
         /** @var string $output */
-        $output = preg_replace('/^(een|één|1)\s+/', '', $interval);
+        $output = preg_replace('/^(een|één|1)\s+/u', '', $interval);
 
-        if (preg_match('/^(een|één|1)( jaar|j| uur|u)/', $interval)) {
+        if (preg_match('/^(een|één|1)( jaar|j| uur|u)/u', $interval)) {
             return "elk $output";
         }
 
