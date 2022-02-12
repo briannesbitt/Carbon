3e01a06de097fbd2880ee785349902bbc644e39e
KyleKatarn <kylekatarnls@gmail.com>
2022-02-12 11:32:11 +0100

    Fix ceil diff with short month

--- src/Carbon/CarbonInterval.php
+++ src/Carbon/CarbonInterval.php
@@ -1654,8 +1654,14 @@ class CarbonInterval extends DateInterval implements CarbonConverterInterface
                 \count($intervalValues->getNonZeroValues()) > $parts &&
                 ($count = \count($keys = array_keys($intervalValues->getValuesSequence()))) > 1
             ) {
+                $index = min($count, $previousCount - 1) - 2;
+
+                if ($index < 0) {
+                    break;
+                }
+
                 $intervalValues = $this->copy()->roundUnit(
-                    $keys[min($count, $previousCount - 1) - 2],
+                    $keys[$index],
                     1,
                     $method
                 );
