ddd375f3563492b394c8594b5a20683c5894049d
KyleKatarn <kylekatarnls@gmail.com>
2022-03-12 19:34:21 +0100

    Make float test with delta tolerance

--- tests/CarbonImmutable/DiffTest.php
+++ tests/CarbonImmutable/DiffTest.php
@@ -1791,7 +1791,7 @@ class DiffTest extends AbstractTestCase
         $d1 = Carbon::parse('2019-06-15 12:34:56.123456');
         $d2 = Carbon::parse('2019-06-16 12:34:56.123455');
 
-        $this->assertSame(-86399.999999, $d2->diffInSeconds($d1));
+        $this->assertEqualsWithDelta(-86399.999999, $d2->diffInSeconds($d1), 0.00001);
     }
 
     public function testNearlyFullDayDiffInMicroseconds()
@@ -1799,7 +1799,7 @@ class DiffTest extends AbstractTestCase
         $d1 = Carbon::parse('2019-06-15 12:34:56.123456');
         $d2 = Carbon::parse('2019-06-16 12:34:56.123455');
 
-        $this->assertSame(-86399999999.0, $d2->diffInMicroseconds($d1));
+        $this->assertEqualsWithDelta(-86399999999.0, $d2->diffInMicroseconds($d1), 0.00001);
     }
 
     public function testExactMonthDiffInSeconds()
