53e07ef67aaf742eb56431de8ecbdc7a82ae4a51
Lucas Michot <lucas@semalead.com>
2022-04-20 18:43:35 +0200

    Fix phpstan

--- src/Carbon/CarbonInterval.php
+++ src/Carbon/CarbonInterval.php
@@ -1435,7 +1435,7 @@ class CarbonInterval extends DateInterval implements CarbonConverterInterface
             ];
         }
 
-        if (isset($altNumbers) && $altNumbers !== true) {
+        if ($altNumbers !== true) {
             $language = new Language($this->locale);
             $altNumbers = \in_array($language->getCode(), (array) $altNumbers, true);
         }
