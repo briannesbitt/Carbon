e23aaab861c3ab0a8c16d1e9304151d57d42a4cc
KyleKatarn <kylekatarnls@gmail.com>
2022-11-01 22:18:22 +0100

    Improve diffInMonths calculation
    Algorithm from https://stackoverflow.com/a/74274307

--- src/Carbon/Traits/Difference.php
+++ src/Carbon/Traits/Difference.php
@@ -191,7 +191,12 @@ trait Difference
     {
         $date = $this->resolveCarbon($date);
 
-        return $this->diffInYears($date, $absolute) * static::MONTHS_PER_YEAR + (int) $this->diff($date, $absolute)->format('%r%m');
+        [$yearStart, $monthStart, $dayStart] = explode('-', $this->format('Y-m-dHisu'));
+        [$yearEnd, $monthEnd, $dayEnd] = explode('-', $date->format('Y-m-dHisu'));
+
+        return ($yearEnd - $yearStart) * static::MONTHS_PER_YEAR + $monthEnd - $monthStart - ($dayStart > $dayEnd ? 1 : 0);
+
+        // return $this->diffInYears($date, $absolute) * static::MONTHS_PER_YEAR + (int) $this->diff($date, $absolute)->format('%r%m');
     }
 
     /**
