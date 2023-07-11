936a216dc4798e2f27c305081ca2f3327f092a4d
kylekatarnls <kylekatarnls@gmail.com>
2023-07-11 15:30:13 +0200

    Exclude locale from getSetting() if set by default
    Fix #2820

--- src/Carbon/Traits/Options.php
+++ src/Carbon/Traits/Options.php
@@ -422,7 +422,7 @@ trait Options
         foreach ($map as $property => $key) {
             $value = $this->$property ?? null;
 
-            if ($value !== null) {
+            if ($value !== null && ($key !== 'locale' || $value !== 'en' || $this->localTranslator)) {
                 $settings[$key] = $value;
             }
         }
@@ -437,7 +437,7 @@ trait Options
      */
     public function __debugInfo()
     {
-        $infos = array_filter(get_object_vars($this), function ($var) {
+        $infos = array_filter(get_object_vars($this), static function ($var) {
             return $var;
         });
 
