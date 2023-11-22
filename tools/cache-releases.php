fed5b965f406dff2dd951add25abfc521a29e2ff
kylekatarnls <kylekatarnls@gmail.com>
2023-11-22 15:44:31 +0100

    Allow to use GITHUB_TOKEN for calls generating the doc

--- tools/cache-releases.php
+++ tools/cache-releases.php
@@ -10,17 +10,33 @@ do {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+    $env = [];
+    $envFile = __DIR__ . '/../.env';
+
+    foreach (file_exists($envFile) ? file($envFile) : [] as $line) {
+        [$key, $value] = array_pad(explode('=', trim($line), 2), 2, null);
+        $env[$key] = $value;
+    }
+
+    if (isset($env['GITHUB_TOKEN'])) {
+        curl_setopt($ch, CURLOPT_HEADER, 'Authorization: Bearer ' . $env['GITHUB_TOKEN']);
+    }
+
     $data = json_decode(curl_exec($ch), JSON_OBJECT_AS_ARRAY);
     curl_close($ch);
+
     foreach ($data as $release) {
         $properties = [
             'tag_name' => $release['tag_name'],
             'created_at' => $release['created_at'],
             'body' => $release['tag_name'],
         ];
+
         if (isset($release['assets'], $release['assets'][0], $release['assets'][0]['asset_url'])) {
             $properties['asset_url'] = $release['assets'][0]['asset_url'];
         }
+
         $releases[] = $properties;
     }
 } while (count($data));
