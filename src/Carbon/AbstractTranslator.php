129700ed449b1f02d70272d2ac802357c8c30c58
kylekatarnls <kylekatarnls@gmail.com>
2024-12-27 10:25:35 +0100

    Validate locale earlier

--- src/Carbon/AbstractTranslator.php
+++ src/Carbon/AbstractTranslator.php
@@ -157,6 +157,8 @@ abstract class AbstractTranslator extends Translation\Translator
             return true;
         }
 
+        $this->assertValidLocale($locale);
+
         foreach ($this->getDirectories() as $directory) {
             $data = @include \sprintf('%s/%s.php', rtrim($directory, '\\/'), $locale);
 
