d379666115cb0d6cdbefedd7605aa6bebe80740b
kylekatarnls <kylekatarnls@gmail.com>
2025-02-19 17:32:48 +0100

    Make all data from sponsors overridable on demand

--- sponsors.php
+++ sponsors.php
@@ -37,14 +37,28 @@ function getHtmlAttribute($rawValue): string
 
 function getOpenCollectiveSponsors(): string
 {
-    $customSponsorImages = [
+    $customSponsorOverride = [
         // For consistency and equity among sponsors, as of now, we kindly ask our sponsors
         // to provide an image having a width/height ratio between 1/1 and 2/1.
         // By default, we'll show the member picture from OpenCollective, and will resize it if bigger
-        // int(OpenCollective.MemberId) => ImageURL
+        662698 => [
+            // alt attribute
+            'name' => 'Non Gamstop Casinos',
+            // title attribute
+            'description' => 'Casinos not on Gamstop',
+            // src attribute
+            'image' => 'https://lgcnews.com/wp-content/uploads/2018/01/LGC-logo-v8-temp.png',
+            // href attribute
+            'website' => 'https://lgcnews.com/',
+        ],
     ];
 
     $members = json_decode(file_get_contents('https://opencollective.com/carbon/members/all.json'), true);
+
+    foreach ($members as &$member) {
+        $member = array_merge($member, $customSponsorOverride[$member['MemberId']] ?? []);
+    }
+
     // Adding sponsors paying via other payment methods
     $members[] = [
         'MemberId' => 1,
